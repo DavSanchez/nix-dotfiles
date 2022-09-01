30b0c4d5c8154cb70155a95ed1734561ce68087b
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-01 23:13:28 +0100

    VSCode settings

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -1,5 +1,24 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
-  home.packages = [ pkgs.vscode ];
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      # pinage404.nix-extension-pack
+      rust-lang.rust-analyzer
+      bungcip.better-toml
+      golang.go
+      haskell.haskell
+      # vscode-icons-team.vscode-icons
+      ms-kubernetes-tools.vscode-kubernetes-tools
+      ms-azuretools.vscode-docker
+      # jeppeandersen.vscode-kafka
+      usernamehw.errorlens
+      eamodio.gitlens
+      # liviuschera.noctis
+      # aaron-bond.better-comments
+    ];
+    #haskell.enable = true;
+    userSettings = lib.importJSON ./settings.json;
+    };
 }
