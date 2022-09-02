5245c6c807296f913c72759fff603a4e7f4e0e30
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-02 03:05:14 +0100

    Formatting

--- modules/dev/haskell.nix
+++ modules/dev/haskell.nix
@@ -3,20 +3,20 @@
 {
   home = {
     packages = with pkgs.haskellPackages; [
-    # Haskell
-    cabal2nix
-    ghc
-    haskell-language-server
-    hoogle
-    cabal-install
-    stack
-  ];
+      # Haskell
+      cabal2nix
+      ghc
+      haskell-language-server
+      hoogle
+      cabal-install
+      stack
+    ];
 
-  # hoogle ghci integration
-  # example> :hoogle <$>
-  file.".ghci".text = ''
-    :def hoogle \x -> return $ ":!hoogle \"" ++ x ++ "\""
-    :def doc \x -> return $ ":!hoogle --info \"" ++ x ++ "\""
-  '';
+    # hoogle ghci integration
+    # example> :hoogle <$>
+    file.".ghci".text = ''
+      :def hoogle \x -> return $ ":!hoogle \"" ++ x ++ "\""
+      :def doc \x -> return $ ":!hoogle --info \"" ++ x ++ "\""
+    '';
   };
 }
