cc0aa4380a6378cd3c68768698b3d2a9358f4a0f
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-04 21:36:40 +0100

    Remove SHA256 for doom-emacs

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -4,7 +4,7 @@ let
   doom-emacs = pkgs.callPackage
     (builtins.fetchTarball {
       url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
-      sha256 = "1nipiwiaypcrkrcls20prw28ly24s6dqd77vbmc12326kb1fy2w3";
+      # sha256 = "1nipiwiaypcrkrcls20prw28ly24s6dqd77vbmc12326kb1fy2w3";
     })
     {
       doomPrivateDir = ./doom.d; # Directory containing your config.el init.el
