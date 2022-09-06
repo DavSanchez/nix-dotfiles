383539e48dc41d20784d73dc2bfd407e8022cdfa
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-07 00:47:33 +0100

    Don't use default size

--- modules/wezterm.nix
+++ modules/wezterm.nix
@@ -10,8 +10,6 @@
     
     return {
       font_size = 14,
-      initial_cols = 120, -- default 80
-      initial_rows = 32, -- default 24
       color_scheme = 'Chalk', -- Chester, Chalk, Ayu Mirage
       window_background_opacity = 0.85,
       hide_tab_bar_if_only_one_tab = true,
