6abbe6a9dca3486a7b22e3c98e922f07aa3c454c
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-12 13:43:40 +0100

    [wezterm] change theme

--- modules/wezterm.nix
+++ modules/wezterm.nix
@@ -14,7 +14,7 @@
         font_size = 14,
         initial_cols = 120, -- default 80
         initial_rows = 32, -- default 24
-        color_scheme = 'Argonaut', -- Argonaut, Cyberdyne, Chalk
+        color_scheme = 'Synthwave (Gogh)', -- Argonaut, Chalk, TokyoNightStorm (Gogh)...
         window_background_opacity = 0.85,
         hide_tab_bar_if_only_one_tab = true,
       }
