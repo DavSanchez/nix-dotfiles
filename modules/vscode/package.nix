10e56868fab84e471e178287ed18803ce158f613
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-13 23:44:56 +0100

    [vscode] use extensions from nixpkgs

--- modules/vscode/package.nix
+++ modules/vscode/package.nix
@@ -50,13 +50,46 @@ in
       let
         # Extension issues and other documentation:
         # https://nixos.wiki/wiki/VSCodium
-        e = import ./extensions.nix;
+        exts = (import ./extensions.nix).extensions;
       in
       {
         enable = true;
-        extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace e.extensions;
         package = pkgs.vscodium; # vscodium.fhs for complex extensions?
         userSettings = lib.importJSON ./settings.json;
+        extensions = with pkgs.vscode-extensions; [
+          ## Extensions present in nixpkgs
+          ## Clashing with complete list appended below
+          hashicorp.terraform
+          matklad.rust-analyzer
+          redhat.vscode-yaml
+          betterthantomorrow.calva
+          bungcip.better-toml
+          christian-kohler.path-intellisense
+          davidanson.vscode-markdownlint
+          dbaeumer.vscode-eslint
+          dhall.dhall-lang
+          dhall.vscode-dhall-lsp-server
+          eamodio.gitlens
+          esbenp.prettier-vscode
+          github.github-vscode-theme
+          golang.go
+          haskell.haskell
+          jakebecker.elixir-ls
+          james-yu.latex-workshop
+          jnoortheen.nix-ide
+          justusadam.language-haskell
+          llvm-vs-code-extensions.vscode-clangd
+          ms-azuretools.vscode-docker
+          ms-python.python
+          ms-python.vscode-pylance
+          ms-vscode-remote.remote-ssh
+          pkief.material-icon-theme
+          pkief.material-product-icons
+          usernamehw.errorlens
+          valentjn.vscode-ltex
+          zxh404.vscode-proto3
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace exts;
       };
   };
 }
