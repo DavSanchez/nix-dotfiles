fa40d83aa0efdbe5e9ed1d64751100ac31ba95ab
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-14 23:22:19 +0100

    [zellij] Configuration, not auto-start (yet)

--- modules/zellij/default.nix
+++ modules/zellij/default.nix
@@ -1,10 +1,18 @@
-{ ... }:
-{
+{ lib, pkgs, ... }:
+
+  {
   programs.zellij = {
     enable = true;
-    settings = { };
+    # settings = { 
+    #   ui.pane_frames.rounded_corners = true;
+    #     no_pane_frames = true;
+    # };
   };
-
   xdg.configFile."zellij/layouts".source = ./layouts;
   # xdg.configFile."layouts".recursive = true;
+
+  # Creating .config/zellij/layouts overwrites
+  # the default config location for mac, hence
+  # we create the config file here as well
+  xdg.configFile."zellij/config.yaml".source = ./config.yaml;
 }
