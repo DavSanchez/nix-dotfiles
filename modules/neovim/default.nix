ff9ea895acf1087348c052d71982a1b0119726bf
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-15 22:33:38 +0100

    [neovim] add several plugins

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -85,12 +85,124 @@
     };
 
     plugins = with pkgs.vimPlugins; [
-      vim-which-key # vimscripts
+      # Appearance
+      bufferline-nvim
+      indent-blankline-nvim
+      lualine-nvim
+      # nvim-alpha # custom plugin
+      nvim-colorizer-lua
+      # nvim-headlines # custom plugin
+      nvim-web-devicons
+
+      # Appearance: Themes
+      dracula-vim
+      one-nvim
+      tokyonight-nvim
+
+      # DAP
+      nvim-dap
+      # nvim-dap-python # custom plugin
+      nvim-dap-ui
+
+      # File Tree
+      nvim-tree-lua
+
+      # Fuzzy Finder
+      cheatsheet-nvim
+      # nvim-better-digraphs # custom plugin
+      telescope-fzf-native-nvim
+      telescope-nvim
+
+      # General Deps
+      nui-nvim
+      plenary-nvim
+      popup-nvim
+
+      # Git
+      gitsigns-nvim
+      vim-fugitive
+
+      # Programming: LSP
+      fidget-nvim
+      lsp_signature-nvim
+      lspkind-nvim
+      null-ls-nvim
+      nvim-lspconfig
+      # nvim-lspsaga # custom plugin
+      # nvim-sqls # custom plugin
+      rust-tools-nvim
+
+      # Progrmming: Treesitter
+      (nvim-treesitter.withPlugins (plugins: with plugins; [
+        tree-sitter-bash
+        tree-sitter-c
+        tree-sitter-css
+        tree-sitter-dockerfile
+        tree-sitter-elm
+        tree-sitter-go
+        tree-sitter-haskell
+        tree-sitter-hcl
+        tree-sitter-html
+        tree-sitter-java
+        tree-sitter-javascript
+        tree-sitter-json
+        tree-sitter-latex
+        tree-sitter-lua
+        tree-sitter-markdown
+        tree-sitter-markdown-inline
+        tree-sitter-nix
+        tree-sitter-python
+        tree-sitter-regex
+        tree-sitter-ruby
+        tree-sitter-rust
+        tree-sitter-scss
+        tree-sitter-toml
+        tree-sitter-tsx
+        tree-sitter-typescript
+        tree-sitter-yaml
+      ]))
+      # nvim-nu # custom plugin
+      nvim-treesitter-refactor
+      nvim-treesitter-textobjects
+      which-key-nvim
+
+      # Programming: Language support
+      crates-nvim
+      # nvim-yuck # custom plugin
+
+      ## Programming: Autocompletion setup
+      nvim-cmp
+      cmp-buffer
+      cmp-calc
+      cmp-nvim-lsp
+      cmp-nvim-lua
+      cmp-path
+      cmp-treesitter
+      cmp-vsnip
+      vim-vsnip
+      vim-vsnip-integ
+
+      ## Project management
+      direnv-vim
+      project-nvim
+
+      # Text Helpers
+      # nvim-regexplainer # custom plugin
+      todo-comments-nvim
+      venn-nvim
+      vim-haskellConcealPlus
+      vim-table-mode
+
+      # Text objects
+      nvim-autopairs
+      nvim-comment
+      nvim-surround
+
       vim-haskellConcealPlus
       vim-nix
       lualine-nvim
       nvim-web-devicons
-      # auto-pairs # or coc-pairs
+      auto-pairs
       plenary-nvim
       telescope-nvim
       nerdtree
@@ -102,8 +214,10 @@
       coc-html
       coc-tsserver
       coc-eslint
-      coc-pairs # or auto-pairs
+      # coc-pairs # or auto-pairs
       coc-prettier
+      coc-python
+      coc-rust-analyzer
     ];
 
     extraConfig = ''
