c62b6be397de88735856a541af19620e79205af3
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-15 22:47:19 +0100

    [wezterm] disable default size

--- modules/wezterm.nix
+++ modules/wezterm.nix
@@ -12,8 +12,6 @@
     
       return {
         font_size = 14,
-        initial_cols = 120, -- default 80
-        initial_rows = 32, -- default 24
         color_scheme = 'Synthwave (Gogh)', -- Argonaut, Chalk, TokyoNightStorm (Gogh)...
         window_background_opacity = 0.85,
         hide_tab_bar_if_only_one_tab = true,
