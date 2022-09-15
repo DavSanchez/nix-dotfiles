6222b18d8d5d5458c70c2b7c29976e45f266c1e6
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-15 21:34:15 +0100

    [doom-emacs] Add sha256 for pure builds

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -5,7 +5,7 @@ let
   doom-emacs = pkgs.callPackage
     (builtins.fetchTarball {
       url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
-      # sha256 = "1nipiwiaypcrkrcls20prw28ly24s6dqd77vbmc12326kb1fy2w3";
+      sha256 = "sha256:17d5fpsk7nj41zgd74p455wfiqx6x9xx3smdrlg7wklir5f92r2c";
     })
     {
       doomPrivateDir = ./doom.d; # Directory containing your config.el init.el
