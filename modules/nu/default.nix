c2c9076d998b903bee1961d547a8457b403fde1c
David Sánchez <david.sanchez.lt@gmail.com>
2022-09-15 17:56:47 +0100

    [nu] config file locations for mac

--- modules/nu/default.nix
+++ modules/nu/default.nix
@@ -1,9 +1,18 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
-{
-  programs.nushell = {
-    enable = true;
-    configFile.source = ./config.nu;
-    envFile.source = ./env.nu;
-  };
-}
+lib.mkMerge [
+  {
+    programs.nushell = {
+      enable = true;
+      configFile.source = ./config.nu;
+      envFile.source = ./env.nu;
+    };
+  }
+  # Until https://github.com/nushell/nushell/issues/893 is solved...
+  (lib.mkIf pkgs.stdenv.isDarwin {
+    home.file = {
+      "Library/Application Support/nushell/config.nu".source = ./config.nu;
+      "Library/Application Support/nushell/env.nu".source = ./env.nu;
+    };
+  })
+]
