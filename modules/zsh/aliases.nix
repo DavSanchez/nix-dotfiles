651f10f01c77e615c9f4217ae4b40c5197dd95f8
David Sánchez <david.sanchez.lt@gmail.com>
2022-10-08 00:01:47 +0100

    [zsh] use plugins for aliases

--- modules/zsh/aliases.nix
+++ modules/zsh/aliases.nix
@@ -1,17 +1,15 @@
 {
   # I don't like the default but my hand just types it
   ":q" = "exit";
-  vi = "nvim";
-  vim = "nvim";
   top = "btm";
   htop = "btm";
   ytop = "btm";
-  cat = "bat --plain";
+  # cat = "bat --plain";
 
   # docker-compose;
-  dc = "docker-compose";
-  dcu = "docker-compose up --build";
-  dcl = "docker-compose logs -f";
+  # dc = "docker-compose";
+  # dcu = "docker-compose up --build";
+  # dcl = "docker-compose logs -f";
   dcisolated = "docker-compose up --build --no-deps consul_common common-redis common_db";
 
   # Navigation;
@@ -21,18 +19,18 @@
   # "....." = "cd ../../../..";
 
   # git;
-  gl = "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-  gc = "git commit";
-  gco = "git checkout";
-  gs = "git status";
-  gca = "git commit --amend";
-  gd = "git diff";
-  gdc = "git diff --cached";
-  gir = "git rebase -i";
+  # gl = "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+  # gc = "git commit";
+  # gco = "git checkout";
+  # gs = "git status";
+  # gca = "git commit --amend";
+  # gd = "git diff";
+  # gdc = "git diff --cached";
+  # gir = "git rebase -i";
   gpr = "gh pr create";
   gdpr = "gh pr create --draft";
   gppr = "git push origin HEAD && gh pr create --fill";
-  gsur = "git submodule update --remote";
+  # gsur = "git submodule update --remote";
 
   # Grep;
   grep = "grep --color=auto";
