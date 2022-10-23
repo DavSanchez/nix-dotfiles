0a5aa25277678bcee30501733de3da94d12601a7
David Sánchez <david.sanchez.lt@gmail.com>
2022-10-23 18:15:16 +0100

    [helix] editor config

--- modules/helix.nix
+++ modules/helix.nix
@@ -2,8 +2,14 @@ _: {
   programs.helix = {
     enable = true;
     settings = {
-      editor.lsp.display-messages = true;
-      theme = "onedark";
+      editor = {
+        lsp.display-messages = true;
+        color-modes = true;
+        statusline = {
+          left = ["mode" "spinner"];
+        };
+      };
+      theme = "onedarker";
       # keys.normal = {
       #   space.space = "file_picker";
       #   space.w = ":w";
