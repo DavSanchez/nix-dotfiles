a03cb31949230ef31bc9a9b64bd0a85c8a9a042b
David Sánchez <david.sanchez.lt@gmail.com>
2022-10-23 20:23:07 +0100

    [helix] customize options

--- modules/helix.nix
+++ modules/helix.nix
@@ -3,13 +3,14 @@ _: {
     enable = true;
     settings = {
       editor = {
+        line-number = "relative";
         lsp.display-messages = true;
         color-modes = true;
         statusline = {
           left = ["mode" "spinner"];
         };
       };
-      # theme = "onedarker";
+      theme = "catppuccin_mocha";
       # keys.normal = {
       #   space.space = "file_picker";
       #   space.w = ":w";
