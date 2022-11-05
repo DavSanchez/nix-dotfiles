2f3187f7c58b0558c16d9d30dcb0921738f116ad
David Sánchez <david.sanchez.lt@gmail.com>
2022-11-05 23:56:56 +0000

    [emacs] update doom-emacs sha256

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -5,7 +5,7 @@ let
     pkgs.callPackage
     (builtins.fetchTarball {
       url = "https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz";
-      sha256 = "sha256:03w6nkyc72j4rszh1iyqb5p3n28w661bgc7dix6i5aygkf3sn86b";
+      sha256 = "sha256:129lhs6jfg3bjskaya943767gfwzkb64iz5xdpcf8vw49151r90r";
     })
     {
       doomPrivateDir = ./doom.d; # Directory containing your config.el init.el
