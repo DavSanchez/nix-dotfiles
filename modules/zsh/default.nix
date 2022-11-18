f194cab06bf5a9af07eb52c93e4cda1683b5f3fb
David Sánchez <david.sanchez.lt@gmail.com>
2022-11-18 23:35:06 +0000

    Refactor strings

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -1,10 +1,9 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
+{ config
+, lib
+, pkgs
+, ...
 }: {
-  home.packages = with pkgs; [];
+  home.packages = with pkgs; [ ];
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -64,35 +63,32 @@
     zplug = {
       enable = true;
       plugins = [
-        {name = "chisui/zsh-nix-shell";}
-        {name = "MichaelAquilina/zsh-you-should-use";}
-        {name = "wfxr/formarks";}
-        {name = "hlissner/zsh-autopair"; tags = ["defer:2"];}
+        { name = "chisui/zsh-nix-shell"; }
+        { name = "MichaelAquilina/zsh-you-should-use"; }
+        { name = "wfxr/formarks"; }
+        { name = "hlissner/zsh-autopair"; tags = [ "defer:2" ]; }
       ];
     };
 
-    initExtraBeforeCompInit =
-      ''
-        ${builtins.readFile ./session_variables.zsh}
-      ''
-      + lib.optionalString pkgs.stdenv.isDarwin (builtins.readFile ./session_variables.mac.zsh)
-      + ''
+    initExtraBeforeCompInit = ''
+      ${builtins.readFile ./session_variables.zsh}
+        
+      ${lib.optionalString pkgs.stdenv.isDarwin (builtins.readFile ./session_variables.mac.zsh)}
 
-        ${builtins.readFile ./functions.zsh}
+      ${builtins.readFile ./functions.zsh}
 
-        bindkey -M vicmd 'k' history-beginning-search-backward
-        bindkey -M vicmd 'j' history-beginning-search-forward
+      bindkey -M vicmd 'k' history-beginning-search-backward
+      bindkey -M vicmd 'j' history-beginning-search-forward
 
-        # ssh-agent (oh-my-zsh plugin) settings should be added before OMZ is sourced
-        # lazy load (https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/ssh-agent#lazy)
-        # zstyle :omz:plugins:ssh-agent lazy yes
-      '';
+      # ssh-agent (oh-my-zsh plugin) settings should be added before OMZ is sourced
+      # lazy load (https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/ssh-agent#lazy)
+      # zstyle :omz:plugins:ssh-agent lazy yes
+    '';
 
     # envExtra = '' '';
 
-    profileExtra =
-      ''''
-      + lib.optionalString pkgs.stdenv.isDarwin ''
+    profileExtra = ''
+      ${lib.optionalString pkgs.stdenv.isDarwin ''
         # Use path_helper
         if [ -x /usr/libexec/path_helper ]; then
           eval `/usr/libexec/path_helper -s`
@@ -105,7 +101,8 @@
         FPATH="$(brew --prefix)/share/zsh/site-functions:$FPATH"
         # Haskell for ARM needs to have LLVM available (Not needed now?)
         # export PATH="$(brew --prefix llvm)/bin:${"\${PATH}"}"
-      '';
+      ''}
+    '';
 
     initExtra = ''
       # Do not add command to history if prepended by space
