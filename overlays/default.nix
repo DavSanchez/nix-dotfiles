696ab0afbaab0e063953c6b73dd44480b854e96e
David Sánchez <david.sanchez.lt@gmail.com>
2022-12-01 20:33:51 +0000

    Remove M1 overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,23 +1,4 @@
 # This file defines overlays
-let
-  inherit (inputs.darwin.lib) darwinSystem;
-  inherit (inputs.nixpkgs.lib) attrValues optionalAttrs singleton;
-  # Configuration for `nixpkgs`
-  nixpkgsConfig = {
-    overlays =
-      attrValues self.overlays
-      ++ singleton (
-        # Sub in x86 version of packages that don't build on Apple Silicon yet
-        final: prev: (optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-          inherit
-            (final.pkgs-x86)
-            # Add packages not available in aarch64-darwin:
-            purescript
-            ;
-        })
-      );
-  };
-in
 {
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
@@ -29,13 +10,5 @@ in
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
-    apple-silicon =
-      optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-        # Add access to x86 packages system is running Apple Silicon
-        pkgs-x86 = import inputs.nixpkgs {
-          system = "x86_64-darwin";
-          inherit (nixpkgsConfig) config;
-        };
-      };
   };
 }
