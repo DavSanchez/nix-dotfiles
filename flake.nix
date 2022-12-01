1a8fe250cbd9ecdcea9b06f5a663449c413b84bb
David Sánchez <david.sanchez.lt@gmail.com>
2022-12-01 19:00:35 +0000

    Copy new structure to top-level

--- flake.nix
+++ flake.nix
@@ -1,248 +1,110 @@
 {
-  description = "Nix configurations of David";
+  description = "DavSanchez's Nix configs";
 
   inputs = {
-    # Package sets
-    # nixos-2205.url = "github:nixos/nixpkgs/nixos-22.05";
-    # nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # nixpkgs-2205-darwin.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    # master.url = "github:nixos/nixpkgs/master";
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Environment/system management
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-    darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    devshell.url = "github:numtide/devshell";
-    flake-utils.url = "github:numtide/flake-utils";
+    # nix-darwin
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    # TODO: Add any other flake you might need
+    hardware.url = "github:nixos/nixos-hardware";
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+
+    # Shameless plug: looking for a way to nixify your themes and make
+    # everything match nicely? Try nix-colors!
+    nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs =
-    { self
-    , nixpkgs-unstable
-    , home-manager
-    , flake-utils
-    , devshell
-    , darwin
-    , ...
-    } @ inputs:
+  outputs = { self, nixpkgs, home-manager, darwin, ... }@inputs:
     let
-      inherit (darwin.lib) darwinSystem;
-      inherit (nixpkgs-unstable.lib) attrValues optionalAttrs singleton;
-
-      # Configuration for `nixpkgs`
-      nixpkgsConfig = {
-        config = { allowUnfree = true; };
-        overlays =
-          attrValues self.overlays
-          ++ singleton (
-            # Sub in x86 version of packages that don't build on Apple Silicon yet
-            final: prev: (optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-              inherit
-                (final.pkgs-x86)
-                # Add packages not available in aarch64-darwin:
-                purescript
-                ;
-            })
-          );
-      };
+      inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
     in
-    {
-      # macOS systems using nix-darwin
-      darwinConfigurations = {
-        "Davids-MacBook-Pro" = darwinSystem {
-          system = "aarch64-darwin";
-          modules = [
-            ./system/mbp/darwin-configuration.nix
-            home-manager.darwinModules.home-manager
-            {
-              nixpkgs = nixpkgsConfig;
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.david = import ./home.nix;
-                # Optionally, use home-manager.extraSpecialArgs to pass
-                # arguments to home.nix
-                extraSpecialArgs = { inherit (inputs) nix-doom-emacs; };
-              };
-            }
-          ];
-        };
-        "Davids-Mac-Mini" = darwinSystem {
-          system = "aarch64-darwin";
-          modules = [
-            ./system/mini/darwin-configuration.nix
-            home-manager.darwinModules.home-manager
-            {
-              nixpkgs = nixpkgsConfig;
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.david = import ./home.nix;
-                extraSpecialArgs = { inherit (inputs) nix-doom-emacs; };
-              };
-            }
-          ];
-        };
-      };
+    rec {
+      # Your custom packages
+      # Acessible through 'nix build', 'nix shell', etc
+      packages = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./pkgs { inherit pkgs; }
+      );
+      # Devshell for bootstrapping
+      # Acessible through 'nix develop' or 'nix-shell' (legacy)
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./shell.nix { inherit pkgs; }
+      );
 
-      overlays = {
-        # Overlay useful on Macs with Apple Silicon
-        apple-silicon = final: prev:
-          optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-            # Add access to x86 packages system is running Apple Silicon
-            pkgs-x86 = import nixpkgs-unstable {
-              system = "x86_64-darwin";
-              inherit (nixpkgsConfig) config;
-            };
-          };
-      };
+      # Your custom packages and modifications, exported as overlays
+      overlays = import ./overlays;
+      # Reusable nixos modules you might want to export
+      # These are usually stuff you would upstream into nixpkgs
+      nixosModules = import ./modules/nixos;
+      # Reusable nix-darwin modules you might want to export
+      # These are usually stuff you would upstream into nix-darwin
+      darwinModules = import ./modules/darwin;
+      # Reusable home-manager modules you might want to export
+      # These are usually stuff you would upstream into home-manager
+      homeManagerModules = import ./modules/home-manager;
 
       templates = import ./templates;
-    }
-    // flake-utils.lib.eachDefaultSystem
-      (system:
-      let
-        pkgs = import nixpkgs-unstable {
-          inherit system;
-          overlays = [ devshell.overlay ];
-        };
-      in
-      {
-        formatter = pkgs.alejandra;
-        # Trying devShells and devshell as a better alternative to Makefile.
-        # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-develop.html
-        # https://github.com/numtide/devshell
-        devShells.default = pkgs.devshell.mkShell {
-          devshell.motd = ''
-            {bold}{14}🔨 My home configs 🔨{reset}
-            $(type -p menu &>/dev/null && menu)
-          '';
 
-          commands = [
-            # # --- Fun ---
-            # {
-            #   name = "dev:hello";
-            #   category = "Fun";
-            #   help = "Print a nice hello world";
-            #   command = ''
-            #     nix run '.#figlet' -- -f isometric1 -c "Hello World"
-            #   '';
-            # }
+      # nixosConfigurations = {
+      #   # FIXME replace with your hostname
+      #   your-hostname = nixpkgs.lib.nixosSystem {
+      #     specialArgs = { inherit inputs outputs; };
+      #     modules = [
+      #       # > Our main nixos configuration file <
+      #       ./nixos/configuration.nix
+      #     ];
+      #   };
+      # };
 
-            # --- Initial Setup ---
-            {
-              name = "dev:install";
-              category = "Initial Setup";
-              help = "Install home-manager itself and apply the home configuration";
-              command = ''
-                export HOME_MANAGER_BACKUP_EXT=old
-                nix build --no-link '.#homeConfigurations.<user>.activationPackage'
-                "$(nix path-info '.#homeConfigurations.<user>.activationPackage')"/activate
-                direnv allow
-              '';
-            }
-
-            # --- Flake ---
-            {
-              name = "dev:update";
-              category = "Flake";
-              help = "Update the flake lock file only";
-              command = ''
-                nix flake update
-              '';
-            }
-
-            # --- Home Environment ---
-            {
-              name = "dev:ls-pkg";
-              category = "Home";
-              help = "List all packages installed in home-manager-path";
-              command = ''
-                home-manager packages
-              '';
-            }
-            {
-              name = "dev:ls-gen";
-              category = "Home";
-              help = "List all home environment generations";
-              command = ''
-                home-manager generations
-              '';
-            }
-
-            # --- Darwin ---
-            {
-              name = "dev:switch_mbp";
-              category = "Darwin";
-              help = "Switch darwin to rebuild and apply `darwin-configuration.nix` changes";
-              command = ''
-                nix build ".#darwinConfigurations.Davids-MacBook-Pro.system"
-                ./result/sw/bin/darwin-rebuild switch --flake ".#Davids-MacBook-Pro"
-              '';
-            }
-
-            {
-              name = "dev:switch_mini";
-              category = "Darwin";
-              help = "Switch darwin to rebuild and apply `darwin-configuration.nix` changes";
-              command = ''
-                nix build ".#darwinConfigurations.Davids-Mac-Mini.system"
-                ./result/sw/bin/darwin-rebuild switch --flake ".#Davids-Mac-Mini"
-              '';
-            }
-
-            # # --- NixOS ---
-            # {
-            #   name = "dev:os-switch";
-            #   category = "NixOS";
-            #   help = "Switch nixos to rebuild and apply `configuration.nix` changes";
-            #   command = ''
-            #     sudo nixos-rebuild switch --flake '.#nixos' --impure
-            #   '';
-            # }
-
-            # --- Utility ---
-            {
-              name = "dev:fmt";
-              category = "Utility";
-              help = "Format nix files";
-              command = ''
-                nix fmt
-              '';
-            }
-            {
-              name = "dev:gc";
-              category = "Utility";
-              help = "Garbage collection";
-              command = ''
-                sudo nix-collect-garbage
-              '';
-            }
-            {
-              name = "dev:gc-stale";
-              category = "Utility";
-              help = ''Perform garbage collection and delete all generations older than 5 days'';
-              command = ''
-                sudo nix-collect-garbage -d --delete-older-than 5d
-              '';
-            }
-            {
-              name = "dev:gc-all";
-              category = "Utility";
-              help = ''Perform garbage collection and delete all old generations'';
-              command = ''
-                sudo nix-collect-garbage -d
-              '';
-            }
+      # macOS systems using nix-darwin
+      darwinConfigurations = {
+        "Davids-MacBook-Pro" = darwin.lib.darwinSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/mbp/darwin-configuration.nix
           ];
         };
-      });
+        "Davids-Mac-Mini" = darwin.lib.darwinSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/mini/darwin-configuration.nix
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        "david@mbp" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            # > Our main home-manager configuration file <
+            ./home-manager/darwin-home.nix
+          ];
+        };
+        "david@mini" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            # > Our main home-manager configuration file <
+            ./home-manager/darwin-home.nix
+          ];
+        };
+      };
+    };
 }
---
+++ hosts/mbp/darwin-configuration.nix
@@ -0,0 +1,213 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces ~/.nixpkgs/darwin-configuration.nix)
+
+{ pkgs
+, config
+, lib
+, inputs
+, ...
+}: {
+  # You can import other nix-darwin modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/darwin):
+    # outputs.darwinModules.example
+
+    # Or modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays your own flake exports (from overlays dir):
+      # outputs.overlays.modifications
+      # outputs.overlays.additions
+
+      # Or overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    package = pkgs.nixUnstable;
+
+    settings = {
+      trusted-users = [ "root" "david" ]; # For groups prepend @: "@admin"
+
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+    };
+    extraOptions =
+      ''
+        auto-optimise-store = true
+        experimental-features = nix-command flakes
+      ''
+      + lib.optionalString (pkgs.system == "aarch64-darwin") ''
+        extra-platforms = x86_64-darwin aarch64-darwin
+      '';
+    gc = {
+      automatic = true;
+      interval = { Day = 7; };
+    };
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [
+    pkgs.vim
+  ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs = {
+    zsh.enable = true; # default shell on Catalina+
+    nix-index.enable = true;
+    # fish.enable = true;
+  };
+
+  users.users = {
+    david = {
+      name = "david";
+      home = "/Users/david";
+    };
+  };
+
+  homebrew = {
+    enable = true;
+    global.brewfile = true;
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true; # This defaults to false so calls are idempotent.
+    onActivation.cleanup = "zap";
+
+    taps = [
+      "homebrew/cask"
+      "homebrew/cask-drivers"
+      "homebrew/cask-versions"
+    ];
+    casks = [
+      "adobe-acrobat-reader"
+      "amethyst"
+      "authy"
+      "brave-browser"
+      # "dash" # Haven't used it
+      "diffusionbee" # Let's have some fun!
+      "discord"
+      "disk-inventory-x"
+      "firefox"
+      "ghidra"
+      "gqrx"
+      "handbrake"
+      "imhex"
+      "inso"
+      "insomnia"
+      "krita"
+      "little-snitch"
+      "logseq"
+      "openemu"
+      "openra"
+      # "orion" # Alternative WebKit browser
+      "protonmail-bridge"
+      "protonvpn"
+      "qflipper"
+      # "raycast" # I don't find it useful at the moment
+      "remarkable"
+      "secretive"
+      # "shortcat" # Manipulate macOS masterfully, minus the mouse
+      "sonic-pi"
+      "stats"
+      "steam"
+      "synthesia"
+      "tor-browser"
+      "transmission"
+      "vlc"
+      "warp"
+      "wireshark"
+      "xld"
+      "xquartz" # X11 applications on macOS
+      "yacreader"
+      "zotero"
+    ];
+    brews = [
+      "bash"
+      "conan"
+      "whalebrew"
+      "pam-reattach" # https://github.com/fabianishere/pam_reattach
+      # "terraform-rover"
+    ];
+    masApps = {
+      "AdGuard for Safari" = 1440147259;
+      "Amphetamine" = 937984704;
+      "GarageBand" = 682658836;
+      "Gemini 2" = 1090488118;
+      "iMovie" = 408981434;
+      "Keynote" = 409183694;
+      "Logic Pro" = 634148309;
+      "MainStage" = 634159523;
+      "Microsoft Remote Desktop" = 1295203466;
+      "Numbers" = 409203825;
+      "Pages" = 409201541;
+      "Reeder" = 1529448980;
+      "Spring for Twitter" = 1508706541;
+      "The Unarchiver" = 425424353;
+      "UTM" = 1538878817;
+      "WhatsApp" = 1147396723;
+      "Xcode" = 497799835;
+    };
+    # extraConfig = '' '';
+    # whalebrews = [ ];
+  };
+
+  ## Other configs
+  # Enable sudo authentication with Touch ID
+  security.pam.enableSudoTouchIdAuth = true;
+
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.mru-spaces = false;
+  system.defaults.dock.orientation = "right";
+  # system.defaults.dock.showhidden = true;
+  system.defaults.finder.AppleShowAllExtensions = true;
+  # system.defaults.finder.AppleShowAllFiles = true;
+  system.defaults.finder.ShowPathbar = true;
+  system.defaults.finder.ShowStatusBar = true;
+  # system.keyboard.enableKeyMapping = true;
+  # system.keyboard.remapCapsLockToControl = true;
+  system.defaults.magicmouse.MouseButtonMode = "TwoButton";
+
+  # fonts.fontDir.enable = true; # Ventura makes this segfault ?
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
