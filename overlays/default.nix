bb1c238d085aff754a0cf2bf776644a81cff7a54
David Sánchez <david.sanchez.lt@gmail.com>
2022-12-02 00:01:45 +0000

    Create overlay for x86_64-darwin availability

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,9 +6,24 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
-  };
+  modifications = final: prev:
+    let
+      x86DarwinPkgs = (import ../nixpkgs.nix { system = "x86_64-darwin"; }).pkgs;
+      mkDarwinX86 = pkgname:
+        prev.lib.optionalAttrs (prev.stdenv.system == "aarch64-darwin") x86DarwinPkgs.${pkgname};
+    in
+    {
+      # example = prev.example.overrideAttrs (oldAttrs: rec {
+      # ...
+      # });
+      # apple-silicon = prev.optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+      #   # Add access to x86 packages system is running Apple Silicon
+      #   pkgs-x86 = import inputs.nixpkgs-unstable {
+      #     system = "x86_64-darwin";
+      #     # inherit (nixpkgsConfig) config;
+      #   };
+      # };
+
+      purescript = mkDarwinX86 "purescript";
+    };
 }
