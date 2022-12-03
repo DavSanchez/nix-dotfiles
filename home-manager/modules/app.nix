85da51fc5480a94b32783e453a75d1525a0e65fd
David Sánchez <davidslt+git@pm.me>
2022-12-03 04:36:14 +0000

    Use zsh with kitty

--- home-manager/modules/app.nix
+++ home-manager/modules/app.nix
@@ -29,7 +29,9 @@
     kitty = {
       enable = true;
       settings = {
-        shell = "/bin/bash --login";
+        # shell = "/bin/bash --login";
+        font_family = "JetBrainsMono Nerd Font";
+        font_size = 14;
       };
     };
   };
