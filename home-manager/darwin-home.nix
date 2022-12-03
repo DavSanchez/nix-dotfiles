81844c814f48ac0d2ae80d367ea9ad428773b2a6
David Sánchez <davidslt+git@pm.me>
2022-12-03 03:49:25 +0000

    Add own overlays to home-manager

--- home-manager/darwin-home.nix
+++ home-manager/darwin-home.nix
@@ -35,8 +35,9 @@
     # You can add overlays here
     overlays = [
       # If you want to use overlays your own flake exports (from overlays dir):
-      outputs.overlays.modifications
       # outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.additions
 
       # Or overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
