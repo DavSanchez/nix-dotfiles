8dc291df722d46f304fba7e2291449be3727c31e
David Sánchez <davidslt+git@pm.me>
2023-02-08 22:57:37 +0000

    Add some fish functions

--- home-manager/modules/fish/default.nix
+++ home-manager/modules/fish/default.nix
@@ -2,5 +2,12 @@
 {
   programs.fish = {
     enable = true;
+    functions = {
+      __fish_command_not_found_handler = {
+        body = "__fish_default_command_not_found_handler $argv[1]";
+        onEvent = "fish_command_not_found";
+      };
+      gitignore = "curl -sL https://www.gitignore.io/api/$argv";
+    };
   };
 }
