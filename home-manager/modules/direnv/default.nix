9be116c780bc62dba3d3ed2baf5140da7aad1dea
David Sánchez <davidslt+git@pm.me>
2023-02-08 21:17:41 +0000

    Enable zsh integration for direnv

--- home-manager/modules/direnv/default.nix
+++ home-manager/modules/direnv/default.nix
@@ -2,6 +2,7 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    enableZshIntegration = true;
     # stdlib = ''
     #   ${builtins.readFile ./project_layouts/poetry.sh}
     # '';
