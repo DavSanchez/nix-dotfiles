581d0840926a3efe92afb2a90cc3636b8a43d618
David Sánchez <david.sanchez.lt@gmail.com>
2023-02-11 01:29:29 +0000

    Disable nix-index at system level

--- hosts/mbp/darwin-configuration.nix
+++ hosts/mbp/darwin-configuration.nix
@@ -93,7 +93,6 @@
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs = {
     zsh.enable = true; # default shell on Catalina+
-    nix-index.enable = true;
     # fish.enable = true;
     bash = {
       enable = true;
