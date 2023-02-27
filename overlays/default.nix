8d291e3766640b4962be33f281ec35c3ae7aa784
David Sánchez <davidslt+git@pm.me>
2023-02-27 15:20:48 +0000

    Add stable pkgs to overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,6 +2,15 @@
 rec {
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
+  
+  # When applied, the stable nixpkgs set (declared in the flake inputs) will
+  # be accessible through 'pkgs.stable'
+  stable-packages = final: _prev: {
+    stable = import inputs.nixpkgs-unstable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
 
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
