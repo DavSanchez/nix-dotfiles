c81373fc74496343379f7ec0ceee31d1e84aec0d
David Sánchez <davidslt+git@pm.me>
2023-02-27 15:32:21 +0000

    Fix stable nixpkgs import

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,7 +6,7 @@ rec {
   # When applied, the stable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.stable'
   stable-packages = final: _prev: {
-    stable = import inputs.nixpkgs-unstable {
+    stable = import inputs.nixpkgs-stable {
       system = final.system;
       config.allowUnfree = true;
     };
