7eccc8db7e9b086b60f45bf75642dc79261da269
David Sánchez <davidslt+git@pm.me>
2023-03-08 14:37:12 +0000

    Add GoLand

--- home-manager/home-nr.nix
+++ home-manager/home-nr.nix
@@ -65,6 +65,9 @@
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [
+    jetbrains.goland
+  ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
