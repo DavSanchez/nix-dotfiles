6c074a0d52e0b89cf14334c5fdd01fb820bad9ba
David Sánchez <davidsanchez@newrelic.com>
2023-04-26 13:43:11 +0100

    broot: remap panels to shift-direction

--- home-manager/features/cli/terminal.nix
+++ home-manager/features/cli/terminal.nix
@@ -66,6 +66,10 @@
       enableZshIntegration = true;
       enableBashIntegration = true;
       enableFishIntegration = true;
+      settings.verbs = [
+        { invocation = "custom_panel_right"; key = "shift-right"; execution = ":panel_right"; }
+        { invocation = "custom_panel_left"; key = "shift-left"; execution = ":panel_left"; }
+      ];
     };
 
     lf = {
