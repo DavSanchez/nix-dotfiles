06e73d4192271a175b4d7eb5fd923e257f7c262c
David Sánchez <davidslt+git@pm.me>
2023-05-17 00:13:37 +0100

    fix: LESS assignments

--- home-manager/features/zsh/default.nix
+++ home-manager/features/zsh/default.nix
@@ -108,14 +108,14 @@
       ''}
 
       # Less variables (quoted inside sessionVariables so they don't work there)
-      export LESS = "-R";
-      export LESS_TERMCAP_mb = "$'\E[1;31m'"; # begin blink
-      export LESS_TERMCAP_md = "$'\E[1;36m'"; # begin bold
-      export LESS_TERMCAP_me = "$'\E[0m'"; # reset bold/blink
-      export LESS_TERMCAP_so = "$'\E[01;44;33m'"; # begin reverse video
-      export LESS_TERMCAP_se = "$'\E[0m'"; # reset reverse video
-      export LESS_TERMCAP_us = "$'\E[1;32m'"; # begin underline
-      export LESS_TERMCAP_ue = "$'\E[0m'"; # reset underline
+      export LESS=-R
+      export LESS_TERMCAP_mb=$'\E[1;31m' # begin blink
+      export LESS_TERMCAP_md=$'\E[1;36m' # begin bold
+      export LESS_TERMCAP_me=$'\E[0m' # reset bold/blink
+      export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
+      export LESS_TERMCAP_se=$'\E[0m' # reset reverse video
+      export LESS_TERMCAP_us=$'\E[1;32m' # begin underline
+      export LESS_TERMCAP_ue=$'\E[0m' # reset underline
 
       # Prepend nix dirs to PATH so they take precendence
       export PATH="$HOME/.local/bin:$HOME/.nix-profile/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$PATH"
