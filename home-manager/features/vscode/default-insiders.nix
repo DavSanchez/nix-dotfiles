b030d290f09d31fbb301fb76d4a2a52bc7a8ae1e
David Sánchez <davidslt+git@pm.me>
2023-05-20 18:43:30 +0100

    style: formatting

--- home-manager/features/vscode/default-insiders.nix
+++ home-manager/features/vscode/default-insiders.nix
@@ -4,12 +4,12 @@
 # We use a custom module for VSCode
 {
   home.packages = [
-  ((pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
-        src = (builtins.fetchTarball {
-          url = "https://code.visualstudio.com/sha/download?build=insider&os=darwin-universal";
-          sha256 = "sha256:17nmla90ykqr2n192v6gka544zw63g66kzpsn38ihgqf2kkhdg8d";
-        });
-        version = "latest";
-      }))
+    ((pkgs.vscode.override {isInsiders = true;}).overrideAttrs (oldAttrs: rec {
+      src = builtins.fetchTarball {
+        url = "https://code.visualstudio.com/sha/download?build=insider&os=darwin-universal";
+        sha256 = "sha256:17nmla90ykqr2n192v6gka544zw63g66kzpsn38ihgqf2kkhdg8d";
+      };
+      version = "latest";
+    }))
   ];
 }
