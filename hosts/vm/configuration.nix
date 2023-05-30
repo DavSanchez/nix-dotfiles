ba981536a75ae337df5378600437256ce40f2fda
David Sánchez <davidsanchez@newrelic.com>
2023-05-30 10:41:35 +0100

    vm: enable qemu host configs

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -101,6 +101,10 @@
     wget
   ];
 
+  services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true;
+  services.spice-webdavd.enable = true;
+
   services.openssh = {
     enable = true;
     settings = {
