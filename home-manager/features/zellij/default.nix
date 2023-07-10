34b33ba984b0641d6f31ac260493abbeb1b2a98e
David Sánchez <davidslt+git@pm.me>
2023-07-10 15:32:11 +0100

    zellij: resetting config

--- home-manager/features/zellij/default.nix
+++ home-manager/features/zellij/default.nix
@@ -1,16 +1,19 @@
 _: {
   programs.zellij = {
     enable = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableZshIntegration = true;
     # settings = {
     #   ui.pane_frames.rounded_corners = true;
     #     no_pane_frames = true;
     # };
   };
-  xdg.configFile."zellij/layouts".source = ./layouts;
+  # xdg.configFile."zellij/layouts".source = ./layouts;
   # xdg.configFile."layouts".recursive = true;
 
   # Creating .config/zellij/layouts overwrites
   # the default config location for mac, hence
   # we create the config file here as well
-  xdg.configFile."zellij/config.yaml".source = ./config.yaml;
+  # xdg.configFile."zellij/config.yaml".source = ./config.yaml;
 }
