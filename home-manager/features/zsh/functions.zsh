910d2fe8c61fd9a11b62c4d61e722b145c3f5820
David Sánchez <davidsanchez@newrelic.com>
2023-07-25 10:36:42 +0100

    zsh: add zellij functions

---
+++ home-manager/features/zsh/functions.zsh
@@ -0,0 +1,5 @@
+# Zellij aliases (not added with completion. Check zellij derivation and zsh completion load procedure to troubleshoot)
+function zr () { zellij run --name "$*" -- zsh -ic "$*";}
+function zrf () { zellij run --name "$*" --floating -- zsh -ic "$*";}
+function ze () { zellij edit "$*";}
+function zef () { zellij edit --floating "$*";}
