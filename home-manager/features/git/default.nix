777a5bca64c36b0545a82455d57472d4f13df343
David Sánchez <davidslt+git@pm.me>
2023-08-09 23:52:06 +0100

    feat: use diff-so-fancy and add basic diff alias (#28)
    Co-authored-by: David Sánchez <david.sanchez.lt@gmail.com>

--- home-manager/features/git/default.nix
+++ home-manager/features/git/default.nix
@@ -30,10 +30,18 @@
     };
 
     difftastic = {
-      enable = true;
+      enable = false;
       background = "dark";
       color = "auto";
       display = "side-by-side";
+    };
+
+    diff-so-fancy = {
+      enable = true;
+      pagerOpts = [
+        "--tabs=4"
+        "-RFX"
+      ];
     };
 
     extraConfig = {
@@ -42,12 +50,49 @@
       # core.editor = "hx";
       pull.ff = "only";
       pull.rebase = true;
+
+      pager.log = "diff-so-fancy | less --tabs=4 -RFX"; # delta
+      pager.reflog = "diff-so-fancy | less --tabs=4 -RFX"; # delta
+      pager.show = "diff-so-fancy | less --tabs=4 -RFX"; # delta
+      pager.blame = "diff-so-fancy | less --tabs=4 -RFX"; # delta
+
+      diff-highlight = true;
+      color.diff-highlight.oldNormal = "red bold";
+      color.diff-highlight.oldHighlight = "red bold 52";
+      color.diff-highlight.newNormal = "green bold";
+      color.diff-highlight.newHighlight = "green bold 22";
+
+      color.diff.meta = "yellow";
+      color.diff.frag = "magenta bold";
+      color.diff.commit = "yellow bold";
+      color.diff.old = "red bold";
+      color.diff.new = "green bold";
+      color.diff.whitespace = "red reverse";
+
+      color.status.added = "green bold";
+      color.status.changed = "red bold";
+      color.status.untracked = "magenta bold";
+
+      color.branch.current = "yellow bold";
+      color.branch.local = "yellow bold";
+      color.branch.remote = "green bold";
+
+      color.ui = "auto";
+
+      color.interactive.prompt = "yellow";
+      color.interactive.header = "cyan bold";
+      color.interactive.help = "cyan bold";
+      color.interactive.error = "red bold";
+      color.interactive.option = "magenta bold";
+      color.interactive.selected = "green bold";
+      color.interactive.prompt = "yellow";
     };
 
     aliases = {
       pushall = "!git remote | xargs -L1 git push --all";
       graph = "log --decorate --oneline --graph";
       add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
+      basicdiff = "diff --no-ext-diff";
     };
   };
 
