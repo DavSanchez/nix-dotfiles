dd2c43a02a156cd4d534b0981ce9ed21abca852f
David Sánchez <davidslt+git@pm.me>
2023-10-09 19:04:00 +0100

    feat: direnv + nushell

--- home-manager/features/direnv/default.nix
+++ home-manager/features/direnv/default.nix
@@ -5,7 +5,7 @@
     enableZshIntegration = true;
     enableBashIntegration = true;
     # enableFishIntegration = true;    # Already enabled by default and read-only
-    # enableNushellIntegration = true; # Already enabled by default and read-only
+    enableNushellIntegration = true;
     # stdlib = ''
     #   ${builtins.readFile ./project_layouts/poetry.sh}
     # '';
