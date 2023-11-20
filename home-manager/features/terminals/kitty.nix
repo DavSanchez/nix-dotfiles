ee245c62af01ad1f303f8ea180d6fca619d6956f
David Sánchez <davidslt+git@pm.me>
2023-11-20 22:05:27 +0000

    kitty: config

--- home-manager/features/terminals/kitty.nix
+++ home-manager/features/terminals/kitty.nix
@@ -1,11 +1,13 @@
-_: {
+{ pkgs, ... }: {
   programs = {
     kitty = {
       enable = true;
       settings = {
         # shell = "/bin/bash --login";
-        font_family = "JetBrainsMono Nerd Font";
+        font_family = "JetBrainsMono Nerd Font Mono";
         font_size = 14;
+        # macos_show_window_title_in = "none";
+        hide_window_decorations = if pkgs.stdenv.isDarwin then "titlebar-only" else "no";
       };
     };
   };
