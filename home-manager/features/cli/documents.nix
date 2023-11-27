9c390cbfc0af782e7c1cf7dac6406ec53fd3311a
David Sánchez <davidslt+git@pm.me>
2023-11-27 22:27:01 +0000

    HM: remove texlive

--- home-manager/features/cli/documents.nix
+++ home-manager/features/cli/documents.nix
@@ -2,7 +2,7 @@
   home.packages = with pkgs; [
     vale
     ## Typesetting (with pygments for minted, pygmentex...)
-    texlive.combined.scheme-full
+    # texlive.combined.scheme-full
     typst
     python311Packages.pygments
     haskellPackages.pandoc-crossref
