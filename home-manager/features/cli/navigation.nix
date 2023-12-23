24197b27882a36e8f3d6be5898d3f59c6f1f1c96
David Sánchez <davidslt+git@pm.me>
2023-12-23 21:50:49 +0000

    home-manager: move options around, disable atuin

--- home-manager/features/cli/navigation.nix
+++ home-manager/features/cli/navigation.nix
@@ -10,46 +10,48 @@
     zoxide
   ];
 
-  yazi = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    enableFishIntegration = true;
-    enableNushellIntegration = true;
-    keymap = {};
-    settings = {};
-    theme = {};
-  };
+  programs = {
+    yazi = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+      enableNushellIntegration = true;
+      keymap = {};
+      settings = {};
+      theme = {};
+    };
 
-  broot = {
-    enable = false;
-    enableZshIntegration = true;
-    enableBashIntegration = true;
-    enableFishIntegration = true;
-    settings.verbs = [
-      {
-        invocation = "custom_panel_right";
-        key = "shift-right";
-        execution = ":panel_right";
-      }
-      {
-        invocation = "custom_panel_left";
-        key = "shift-left";
-        execution = ":panel_left";
-      }
-    ];
-  };
-
-  nnn = {
-    enable = true;
-    package = pkgs.nnn.override {withNerdIcons = true;};
-    extraPackages = with pkgs;
-      [
-        ffmpegthumbnailer
-        mediainfo
-      ]
-      ++ lib.optionals pkgs.stdenv.isLinux [
-        sxiv
+    broot = {
+      enable = false;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      settings.verbs = [
+        {
+          invocation = "custom_panel_right";
+          key = "shift-right";
+          execution = ":panel_right";
+        }
+        {
+          invocation = "custom_panel_left";
+          key = "shift-left";
+          execution = ":panel_left";
+        }
       ];
+    };
+
+    nnn = {
+      enable = true;
+      package = pkgs.nnn.override {withNerdIcons = true;};
+      extraPackages = with pkgs;
+        [
+          ffmpegthumbnailer
+          mediainfo
+        ]
+        ++ lib.optionals pkgs.stdenv.isLinux [
+          sxiv
+        ];
+    };
   };
 }
