0a8a02ef0ff3dde6573754d31bc7f6e0b3a988ed
David Sánchez <davidslt+git@pm.me>
2023-12-27 19:48:12 +0000

    FP: Add F-star

--- home-manager/features/dev/fp.nix
+++ home-manager/features/dev/fp.nix
@@ -4,6 +4,7 @@
     agda # Functional programmingw with dependent types, 2
     elixir # Functional programming for the Erlang VM
     dotnet-sdk # For F-sharp: functional programming for .NET
+    fstar # ML-like functional programming language aimed at program verification
     elmPackages.elm # Functional programming for the frontend
     flix # Functional programming for the JVM
     clojure # Functional programming for the JVM, lisp-like
