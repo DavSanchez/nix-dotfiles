0f64708f903975c00dfc2a5e90811b15f16989c2
David Sánchez <davidslt+git@pm.me>
2024-03-06 23:22:09 +0000

    neovim: disable old cmp config (TODO customize)

--- home-manager/features/neovim/plugins/default.nix
+++ home-manager/features/neovim/plugins/default.nix
@@ -20,39 +20,7 @@
   nix.enable = true;
   nix-develop.enable = true;
   nvim-autopairs.enable = true;
-  nvim-cmp = {
-    enable = true;
-    autoEnableSources = true;
-    sources = [
-      {name = "nvim_lsp";}
-      {name = "path";}
-      {name = "buffer";}
-      # {name = "luasnip";}
-      {name = "codeium";}
-    ];
-
-    mapping = {
-      "<CR>" = "cmp.mapping.confirm({ select = true })";
-      "<Tab>" = {
-        action = ''
-          function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            -- elseif luasnip.expandable() then
-            --   luasnip.expand()
-            -- elseif luasnip.expand_or_jumpable() then
-            --   luasnip.expand_or_jump()
-            elseif check_backspace() then
-              fallback()
-            else
-              fallback()
-            end
-          end
-        '';
-        modes = ["i" "s"];
-      };
-    };
-  };
+  cmp.enable = true;
   oil.enable = true;
   rustaceanvim.enable = true;
   telescope.enable = true;
