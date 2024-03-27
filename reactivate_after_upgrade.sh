ac7515d10490a9591612dea2cbb66f6d67d6836b
David Sánchez <davidslt+git@pm.me>
2024-03-27 16:21:35 +0000

    chore: remove unused script

--- reactivate_after_upgrade.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-sudo mv /etc/bashrc /etc/bashrc.orig
-sudo mv /etc/zshrc /etc/zshrc.orig
-sudo mv /etc/zprofile /etc/zprofile.orig
-sudo /nix/var/nix/profiles/system/activate
-exit  # Start a new shell to reload the environment.
