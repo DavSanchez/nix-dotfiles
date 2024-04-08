2769da8bf0cb1ef01e37a98ea49c6bd99aaa1eb1
David Sánchez <davidslt+git@pm.me>
2024-04-08 23:24:23 +0100

    neovim: extract cmp, add conform

---
+++ home-manager/features/neovim/plugins/cmp.nix
@@ -0,0 +1,42 @@
+{
+  snippet.expand = ''
+    function(args)
+      require('luasnip').lsp_expand(args.body)
+    end
+  '';
+  mapping = {
+    "<C-Space>" = "cmp.mapping.complete()";
+    "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+    "<C-e>" = "cmp.mapping.close()";
+    "<C-f>" = "cmp.mapping.scroll_docs(4)";
+    "<CR>" = "cmp.mapping.confirm({ select = true })";
+    "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+    "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+  };
+  sources = [
+    {
+      name = "nvim_lsp";
+    }
+    {
+      name = "luasnip";
+    }
+    {
+      name = "path";
+    }
+    {
+      name = "buffer";
+    }
+    {
+      name = "conventionalcommits";
+    }
+    {
+      name = "dictionary";
+    }
+    {
+      name = "copilot";
+    }
+    # {
+    # name = "codeium";
+    # }
+  ];
+}
--- home-manager/features/neovim/plugins/default.nix
+++ home-manager/features/neovim/plugins/default.nix
@@ -2,6 +2,7 @@
   barbar.enable = true;
   codeium-nvim.enable = true;
   comment.enable = true;
+  conform-nvim.enable = true;
   copilot-lua = {
     enable = true;
     suggestion.enabled = false;
@@ -25,48 +26,7 @@
   nvim-autopairs.enable = true;
   cmp = {
     enable = true;
-    settings = {
-      snippet.expand = ''
-        function(args)
-          require('luasnip').lsp_expand(args.body)
-        end
-      '';
-      mapping = {
-        "<C-Space>" = "cmp.mapping.complete()";
-        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-        "<C-e>" = "cmp.mapping.close()";
-        "<C-f>" = "cmp.mapping.scroll_docs(4)";
-        "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-      };
-      sources = [
-        {
-          name = "nvim_lsp";
-        }
-        {
-          name = "luasnip";
-        }
-        {
-          name = "path";
-        }
-        {
-          name = "buffer";
-        }
-        {
-          name = "conventionalcommits";
-        }
-        {
-          name = "dictionary";
-        }
-        {
-          name = "copilot";
-        }
-        # {
-        # name = "codeium";
-        # }
-      ];
-    };
+    settings = import ./cmp.nix;
   };
   cmp-path.enable = true;
   cmp-buffer.enable = true;
