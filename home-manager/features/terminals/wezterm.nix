327bbccb4c96cbf3a40705a70aef6385357d799b
David Sánchez <davidslt+git@pm.me>
2024-04-08 23:37:19 +0100

    wezterm: change color scheme

--- home-manager/features/terminals/wezterm.nix
+++ home-manager/features/terminals/wezterm.nix
@@ -8,7 +8,7 @@ _: {
 
       return {
         font_size = 14,
-        color_scheme = 'SynthWave (Gogh)',
+        color_scheme = 'Catppuccin Mocha',
         window_background_opacity = 0.95,
         hide_tab_bar_if_only_one_tab = true,
         -- term = "wezterm",
