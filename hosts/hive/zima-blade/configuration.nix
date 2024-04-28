b913c1ffc6d1b442cd9f1d2721fea78808c55e49
David Sánchez <davidslt+git@pm.me>
2024-04-28 15:10:51 +0100

    hive(zima): enable nfs, mail notifs, etc

--- hosts/hive/zima-blade/configuration.nix
+++ hosts/hive/zima-blade/configuration.nix
@@ -25,13 +25,60 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # ZFS
   boot.supportedFilesystems = ["zfs"];
   boot.zfs.forceImportRoot = false;
+  services.zfs.autoScrub.enable = true;
+  services.zfs.trim.enable = true;
+  # Accessible via network
+  services.nfs.server.enable = true;
+  # Enable mail notifications
+  programs.msmtp = {
+    enable = true;
+    setSendmail = true;
+    defaults = {
+      aliases = "/etc/aliases";
+      port = 465;
+      tls_trust_file = "/etc/ssl/certs/ca-certificates.crt";
+      tls = "on";
+      auth = "login";
+      tls_starttls = "off";
+    };
+    accounts = {
+      default = {
+        host = "mail.example.com"; # CHANGEME
+        passwordeval = "cat /etc/emailpass.txt";
+        user = "user@example.com"; # CHANGEME
+        from = "user@example.com"; # CHANGEME
+      };
+    };
+  };
+  services.zfs.zed.settings = {
+    ZED_DEBUG_LOG = "/tmp/zed.debug.log";
+    ZED_EMAIL_ADDR = [
+      # "root"
+      "zimazfszed.jc5ka@passmail.net"
+    ];
+    ZED_EMAIL_PROG = "${pkgs.msmtp}/bin/msmtp";
+    ZED_EMAIL_OPTS = "@ADDRESS@";
+
+    ZED_NOTIFY_INTERVAL_SECS = 3600;
+    ZED_NOTIFY_VERBOSE = true;
+
+    ZED_USE_ENCLOSURE_LEDS = true;
+    ZED_SCRUB_AFTER_RESILVER = true;
+  };
+  services.zfs.zed.enableMail = false;
 
   networking = {
+    # Required for ZFS
     hostId = "bfbc2f21";
     hostName = "zima-blade";
-    firewall.allowedTCPPorts = [80];
+    firewall.allowedTCPPorts = [
+      80
+      2049 # NFSv4
+    ];
   };
 
   time.timeZone = "Atlantic/Canary";
@@ -65,6 +112,7 @@
       dates = "weekly";
     };
   };
+
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
