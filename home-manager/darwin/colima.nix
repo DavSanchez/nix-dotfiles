9777966f581ee52ea896ed1e5dc28a8a25417a81
David Sánchez <davidslt+git@pm.me>
2024-05-14 21:57:34 +0100

    darwin: colima options for mount type 9p

---
+++ home-manager/darwin/colima.nix
@@ -0,0 +1,22 @@
+{config, ...}: {
+  # lima/colima config for writable mounts: https://github.com/abiosoft/colima/issues/83#issuecomment-1339269542
+  home.file.".colima/_lima/_config/override.yaml".text = ''
+    mountType: 9p
+    mounts:
+      - location: "/Users/${config.home.username}"
+        writable: true
+        9p:
+          securityModel: mapped-xattr
+          cache: mmap
+      - location: "~"
+        writable: true
+        9p:
+          securityModel: mapped-xattr
+          cache: mmap
+      - location: /tmp/colima
+        writable: true
+        9p:
+          securityModel: mapped-xattr
+          cache: mmap
+  '';
+}
