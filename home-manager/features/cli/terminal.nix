056095cafba32cb0deb5477290be1b8050243748
David Sánchez <davidslt+git@pm.me>
2024-05-20 13:44:35 +0100

    home: change fzf theme for rose-pine-moon

--- home-manager/features/cli/terminal.nix
+++ home-manager/features/cli/terminal.nix
@@ -37,6 +37,23 @@
       historyWidgetOptions = ["--sort" "--exact"];
       tmux.enableShellIntegration = true;
       tmux.shellIntegrationOptions = ["-d 40%"];
+      colors = {
+        fg = "#908caa";
+        bg = "#232136";
+        hl = "#ea9a97";
+        "fg+" = "#e0def4";
+        "bg+" = "#393552";
+        "hl+" = "#ea9a97";
+        border = "#44415a";
+        header = "#3e8fb0";
+        gutter = "#232136";
+        spinner = "#f6c177";
+        info = "#9ccfd8";
+        separator = "#44415a";
+        pointer = "#c4a7e7";
+        marker = "#eb6f92";
+        prompt = "#908caa";
+      };
     };
 
     bat = {
