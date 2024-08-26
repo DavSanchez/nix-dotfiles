a19e1eb78e9b72c8f839b5cf80066351b9cb8af4
David Sánchez <davidslt+git@pm.me>
2024-08-26 14:03:45 +0100

    vm: add catppuccin theme for nixos vm

--- hosts/vm/utm-nr/configuration.nix
+++ hosts/vm/utm-nr/configuration.nix
@@ -176,6 +176,11 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  catppuccin = {
+    enable = true;
+    flavor = "mocha";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
