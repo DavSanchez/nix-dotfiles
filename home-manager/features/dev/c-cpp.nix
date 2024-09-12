616fe2ac11a51453ce2b0c4f2ce2b4f94c75dcdb
David Sánchez <davidslt+git@pm.me>
2024-09-13 00:47:41 +0100

    feat(dev): auto-add C tools on non-Darwin

--- home-manager/features/dev/c-cpp.nix
+++ home-manager/features/dev/c-cpp.nix
@@ -1,17 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  home.packages = with pkgs; [
-    gcc
-    rosetta.gdb
+  home.packages =
+    with pkgs;
+    [
+      # cling
+      # ccls
 
-    llvm
-    lldb
+      # gnumake already defined in module top level
+      # cmake
+      # platformio
+    ]
+    ++ lib.optionals (!stdenv.isDarwin) [
+      # Apple already provides these tools in its Command Line Tools
+      # and some other toolchains expect them there, so conflicts often.
+      # Add on-demand via Nix shells if required for Darwin.
+      gcc
 
-    # cling
-    # ccls
-
-    # gnumake already defined in module top level
-    # cmake
-    # platformio
-  ];
+      # llvm
+      lldb
+    ];
 }
