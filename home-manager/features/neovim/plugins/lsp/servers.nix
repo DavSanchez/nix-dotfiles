e33181076e512cbd463d50ce67188617ae82881e
David Sánchez <davidslt+git@pm.me>
2024-10-09 23:25:56 +0100

    neovim: address option renaming

--- home-manager/features/neovim/plugins/lsp/servers.nix
+++ home-manager/features/neovim/plugins/lsp/servers.nix
@@ -2,14 +2,14 @@
   ansiblels.enable = true;
   bashls.enable = true;
   clangd.enable = true;
-  clojure-lsp.enable = true;
-  dhall-lsp-server.enable = true;
+  clojure_lsp.enable = true;
+  dhall_lsp_server.enable = true;
   dockerls.enable = true;
   elixirls.enable = true;
   eslint.enable = true;
   gleam.enable = true;
   gopls.enable = true;
-  helm-ls.enable = true;
+  helm_ls.enable = true;
   hls.enable = true;
   html.enable = true;
   jsonls.enable = true;
@@ -19,14 +19,14 @@
   purescriptls.enable = true;
   pylsp.enable = true;
   pyright.enable = true;
-  rust-analyzer = {
+  rust_analyzer = {
     enable = false; # Using rustaceanvim
     installCargo = false;
     installRustc = false;
   };
   taplo.enable = true;
   terraformls.enable = true;
-  ts-ls.enable = true;
+  ts_ls.enable = true;
   yamlls.enable = true;
   zls.enable = true;
 }
