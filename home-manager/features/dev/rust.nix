e58c942ccf87572cd97a2ebe94c192dbb8a16a62
David Sánchez <davidslt+git@pm.me>
2024-10-15 23:51:55 +0100

    rust: remove rust-analyzer as already installed by rustup

--- home-manager/features/dev/rust.nix
+++ home-manager/features/dev/rust.nix
@@ -3,7 +3,7 @@
   home = {
     packages = with pkgs; [
       rustup
-      rust-analyzer # Langserver
+
       trunk # WASM bundler
       cargo-watch # Watch for changes and run cargo-watch
       cargo-cross
