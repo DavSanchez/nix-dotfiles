495f527e82690bf081da77e4bfe47bcaffc5113b
David Sánchez <davidslt+git@pm.me>
2024-11-26 16:14:37 +0000

    style: unbind unused params

--- hosts/example-configuration.nix
+++ hosts/example-configuration.nix
@@ -2,9 +2,6 @@
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 {
   inputs,
-  lib,
-  config,
-  pkgs,
   ...
 }:
 {
--- hosts/vm/utm-nr/configuration.nix
+++ hosts/vm/utm-nr/configuration.nix
@@ -3,9 +3,7 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 {
-  config,
   pkgs,
-  lib,
   inputs,
   ...
 }:
@@ -137,9 +135,9 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+  environment.systemPackages = [
+    #  pkgs.vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  pkgs.wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
