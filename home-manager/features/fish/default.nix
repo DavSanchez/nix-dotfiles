d8df945c07aef614f147854fc276d5bf01f350d4
David Sánchez <davidslt+git@pm.me>
2024-11-29 00:40:16 +0000

    chore: comment-out function

--- home-manager/features/fish/default.nix
+++ home-manager/features/fish/default.nix
@@ -15,10 +15,10 @@ in
     enable = true;
 
     functions = {
-      __fish_command_not_found_handler = {
-        body = "__fish_default_command_not_found_handler $argv[1]";
-        onEvent = "fish_command_not_found";
-      };
+      # __fish_command_not_found_handler = {
+      #   body = "__fish_default_command_not_found_handler $argv[1]";
+      #   onEvent = "fish_command_not_found";
+      # };
       gitignore = "curl -sL https://www.gitignore.io/api/$argv";
     };
 
