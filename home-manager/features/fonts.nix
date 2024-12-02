f1fe954aa0ab47c96ac7004194a8e90c15cd3e50
David Sánchez <davidslt+git@pm.me>
2024-12-02 18:10:50 +0000

    feat: modify how nerd-fonts are declared

--- home-manager/features/fonts.nix
+++ home-manager/features/fonts.nix
@@ -3,14 +3,13 @@
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
-    (nerdfonts.override {
-      fonts = [
-        "FiraCode"
-        "Hack"
-        "Iosevka"
-        "JetBrainsMono"
-      ];
-    })
+    nerd-fonts.fira-code
+    nerd-fonts.hack
+    nerd-fonts.iosevka
+    nerd-fonts.iosevka-term
+    nerd-fonts.iosevka-term-slab
+    nerd-fonts.jetbrains-mono
+
     fira-code-symbols
     iosevka # Allows for customization via privateBuildPlan
     sketchybar-app-font
