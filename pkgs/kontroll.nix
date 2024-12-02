e06d7170be918c885b628ec019a248bb393b098a
David Sánchez <davidslt+git@pm.me>
2024-12-02 16:05:25 +0000

    style: specify maintainer

--- pkgs/kontroll.nix
+++ pkgs/kontroll.nix
@@ -47,7 +47,7 @@ rustPlatform.buildRustPackage rec {
     description = "Kontroll demonstates how to control the Keymapp API, making it easy to control your ZSA keyboard from the command line and scripts";
     homepage = "https://github.com/zsa/kontroll";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ ];
+    maintainers = [ lib.maintainers.davsanchez ];
     mainProgram = "kontroll";
   };
 }
