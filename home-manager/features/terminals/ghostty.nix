aaf58afc451b5adfef5b0d2d0d28368406151221
David Sánchez <davidslt+git@pm.me>
2025-01-27 22:40:24 +0000

    feat: add Ghostty with home-manager

--- home-manager/features/terminals/ghostty.nix
+++ home-manager/features/terminals/ghostty.nix
@@ -1,16 +1,31 @@
+{ lib, pkgs, ... }:
 {
-  lib,
-  config,
-  ...
-}:
-{
-  xdg.configFile."ghostty/config".text = lib.strings.concatStringsSep "\n" [
-    "font-family = \"Iosevka Term Slab Custom\""
-    "font-size = 14"
-    "theme = catppuccin-mocha"
-    "background-opacity = 0.8"
-    "background-blur-radius = 20"
-    "cursor-click-to-move = true"
-    (lib.optionalString config.programs.fish.enable "command = \"${config.programs.fish.package}/bin/fish\"")
-  ];
+  programs.ghostty = {
+    enable = true;
+    package = pkgs.ghostty-bin;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+
+    installBatSyntax = true;
+    installVimSyntax = true;
+
+    settings =
+      {
+        theme = "catppuccin-mocha";
+        font-family = "Iosevka Term Slab Custom";
+        font-size = 14;
+        background-opacity = 0.8;
+        background-blur-radius = 20;
+        cursor-click-to-move = true;
+        # (lib.optionalString config.programs.fish.enable "command = \"${config.programs.fish.package}/bin/fish\"")
+      }
+      // lib.optionalAttrs pkgs.stdenv.isDarwin {
+        keybind = [
+          "alt+left=unbind"
+          "alt+right=unbind"
+        ];
+        macos-option-as-alt = true;
+      };
+  };
 }
