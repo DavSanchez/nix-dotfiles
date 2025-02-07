c0a0e31a9c2b21bcdc003a36f9a0281058e130db
David Sánchez <davidslt+git@pm.me>
2025-02-07 15:15:05 +0000

    feat(nfs): remove

--- hosts/hive/eter/fs_share.nix
+++ hosts/hive/eter/fs_share.nix
@@ -1,25 +1,4 @@
 _: {
-  # Accessible via network
-  services.nfs.server = {
-    enable = true;
-    # fixed rpc.statd port; for firewall
-    lockdPort = 4001;
-    mountdPort = 4002;
-    statdPort = 4000;
-    # extraNfsdConfig = '''';
-    # see `man exports` for option details
-    exports = ''
-      /seclusium 192.168.8.0/24(fsid=0,no_subtree_check)
-
-      /seclusium/creation 192.168.8.0/24(no_subtree_check,all_squash,anonuid=1000,anongid=100)
-      /seclusium/dimensions 192.168.8.0/24(no_subtree_check,all_squash,anonuid=1000,anongid=100)
-      /seclusium/echoes 192.168.8.102(rw,no_subtree_check,all_squash,anonuid=1000,anongid=100)
-      /seclusium/imagery 192.168.8.0/24(no_subtree_check,all_squash,anonuid=1000,anongid=100)
-      /seclusium/technique 192.168.8.0/24(no_subtree_check,all_squash,anonuid=1000,anongid=100)
-      /seclusium/zg 192.168.8.0/24(no_subtree_check,all_squash,anonuid=1000,anongid=100)
-    '';
-  };
-
   services.samba = {
     enable = true;
     openFirewall = true;
@@ -27,34 +6,15 @@ _: {
       global = {
         "hosts allow" = "192.168.8. 127.0.0.1 localhost";
         "hosts deny" = "0.0.0.0/0";
+        writeable = "no";
       };
-
       # Shares
-      creation = {
-        path = "/seclusium/creation";
-        writeable = "yes";
-      };
-      dimensions = {
-        path = "/seclusium/dimensions";
-        writeable = "no";
-      };
-      echoes = {
-        path = "/seclusium/echoes";
-        writeable = "yes";
-        "hosts allow" = "192.168.8.102";
-      };
-      imagery = {
-        path = "/seclusium/imagery";
-        writeable = "no";
-      };
-      technique = {
-        path = "/seclusium/technique";
-        writeable = "no";
-      };
-      zg = {
-        path = "/seclusium/zg";
-        writeable = "no";
-      };
+      creation.path = "/seclusium/creation";
+      dimensions.path = "/seclusium/dimensions";
+      echoes.path = "/seclusium/echoes";
+      imagery.path = "/seclusium/imagery";
+      technique.path = "/seclusium/technique";
+      zg.path = "/seclusium/zg";
     };
   };
 }
