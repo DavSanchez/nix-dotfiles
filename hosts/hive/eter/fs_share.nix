0b6c3e57521134a3f2737fa71908887fb0fcf3bb
David Sánchez <davidslt+git@pm.me>
2025-02-08 13:50:37 +0000

    feat(smb): reconfigure

--- hosts/hive/eter/fs_share.nix
+++ hosts/hive/eter/fs_share.nix
@@ -6,15 +6,33 @@ _: {
       global = {
         "hosts allow" = "192.168.8. 127.0.0.1 localhost";
         "hosts deny" = "0.0.0.0/0";
-        writeable = "no";
       };
       # Shares
-      creation.path = "/seclusium/creation";
-      dimensions.path = "/seclusium/dimensions";
-      echoes.path = "/seclusium/echoes";
-      imagery.path = "/seclusium/imagery";
-      technique.path = "/seclusium/technique";
-      zg.path = "/seclusium/zg";
+      creation = {
+        path = "/seclusium/creation";
+        writeable = "no";
+      };
+      dimensions = {
+        path = "/seclusium/dimensions";
+        writeable = "no";
+      };
+      echoes = {
+        path = "/seclusium/echoes";
+        writeable = "yes";
+        "hosts allow" = "192.168.8.102";
+      };
+      imagery = {
+        path = "/seclusium/imagery";
+        writeable = "no";
+      };
+      technique = {
+        path = "/seclusium/technique";
+        writeable = "no";
+      };
+      zg = {
+        path = "/seclusium/zg";
+        writeable = "no";
+      };
     };
   };
 }
