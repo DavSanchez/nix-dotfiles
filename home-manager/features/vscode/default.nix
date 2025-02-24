21f3518522ddb4e2180b5048b43b9291508e43f3
David Sánchez <davidslt+git@pm.me>
2025-02-24 12:34:34 +0000

    chore: move vscode configs to default profile

--- home-manager/features/vscode/default.nix
+++ home-manager/features/vscode/default.nix
@@ -6,17 +6,17 @@
   programs.vscode = {
     enable = true;
     # package = pkgs.vscodium; # vscodium.fhs for complex extensions?
-    enableUpdateCheck = true;
-
-    userSettings = (import ./settings.nix) pkgs; # Pass pkgs to reference paths
-    enableExtensionUpdateCheck = true;
     mutableExtensionsDir = true;
 
-    # Extra keybindings leveraging hyper key
-    keybindings = import ./keybindings.nix;
-
-    # Extension issues and other documentation:
-    # https://nixos.wiki/wiki/VSCodium
-    extensions = (import ./extensions.nix) pkgs;
+    profiles.default = {
+      enableUpdateCheck = true;
+      userSettings = (import ./settings.nix) pkgs; # Pass pkgs to reference paths
+      # Extra keybindings leveraging hyper key (to modify)
+      keybindings = import ./keybindings.nix;
+      # Extension issues and other documentation:
+      # https://nixos.wiki/wiki/VSCodium
+      extensions = (import ./extensions.nix) pkgs;
+      enableExtensionUpdateCheck = true;
+    };
   };
 }
