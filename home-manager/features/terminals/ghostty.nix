fe8dfd61d045cb1c5af3c89cd6dcc560307faa6e
David Sánchez <davidslt+git@pm.me>
2025-04-28 12:32:38 +0100

    feat: make ghostty load fish

--- home-manager/features/terminals/ghostty.nix
+++ home-manager/features/terminals/ghostty.nix
@@ -4,16 +4,13 @@
   ...
 }:
 {
-  xdg.configFile."ghostty/config".text =
-    ''
-      font-family = "Iosevka Term Slab Custom"
-      font-size = 14
-      theme = catppuccin-mocha
-      background-opacity = 0.8
-      background-blur-radius = 20
-      cursor-click-to-move = true
-    ''
-    ++ lib.optionalString config.programs.fish.enable ''
-      command = ${config.programs.fish.package}/bin/fish;
-    '';
+  xdg.configFile."ghostty/config".text = lib.strings.concatStrings [
+    "font-family = \"Iosevka Term Slab Custom\""
+    "font-size = 14"
+    "theme = catppuccin-mocha"
+    "background-opacity = 0.8"
+    "background-blur-radius = 20"
+    "cursor-click-to-move = true"
+    (lib.optionalString config.programs.fish.enable "command = ${config.programs.fish.package}/bin/fish")
+  ];
 }
