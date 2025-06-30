7a81bf3555a3be4949a612b220c310c57d0b2d0a
David Sánchez <davidslt+git@pm.me>
2025-06-30 10:44:39 +0100

    fix(zed): disable for now

--- home-manager/features/zed/default.nix
+++ home-manager/features/zed/default.nix
@@ -1,31 +1,27 @@
-{pkgs,...}: {
+{ pkgs, ... }:
+{
   programs.zed-editor = {
-    enable = true;
-
-    extensions = [
-      "nix"
-      "haskell"
-      # "catppuccin-blur"
-    ];
+    enable = false;
+    # extensions = [
+    #   "nix"
+    #   "haskell"
+    #   # "catppuccin-blur"
+    # ];
     # userKeymaps = { };
-    userSettings = {
-      vim_mode = true;
-      vim = {
-        default_mode = "helix_normal";
-      };
-
-      buffer_font_family = "Iosevka Slab Custom";
-      load_direnv = "shell_hook";
-      show_whitespaces = "all";
-      autosave = {
-        after_delay.milliseconds = 1000;
-      };
-      relative_line_numbers = true;
-    };
-    extraPackages = with pkgs; [
-      rust-analyzer
-      haskell-language-server
-      nixd
+    # userSettings = {
+    #   vim_mode = true;
+    #   vim = {
+    #     default_mode = "helix_normal";
+    #   };
+    #   buffer_font_family = "Iosevka Slab Custom";
+    #   load_direnv = "shell_hook";
+    #   show_whitespaces = "all";
+    #   autosave = {
+    #     after_delay.milliseconds = 1000;
+    #   };
+    #   relative_line_numbers = true;
+    # };
+    extraPackages = [
     ];
     installRemoteServer = true;
   };
