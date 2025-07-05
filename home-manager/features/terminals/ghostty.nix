2d5c56202cc3912ed1ea10f32d5754f24827a324
David Sánchez <davidslt+git@pm.me>
2025-07-05 18:42:13 +0100

    override ghostty pkg only on darwin

--- home-manager/features/terminals/ghostty.nix
+++ home-manager/features/terminals/ghostty.nix
@@ -5,35 +5,38 @@
   ...
 }:
 {
-  programs.ghostty = {
-    enable = true;
-    package = pkgs.ghostty-bin;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    enableFishIntegration = true;
+  programs.ghostty =
+    {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
 
-    installBatSyntax = true;
-    installVimSyntax = true;
+      installBatSyntax = true;
+      installVimSyntax = true;
 
-    settings =
-      {
-        theme = "catppuccin-mocha";
-        font-family = "Iosevka Term Slab Custom";
-        font-size = 14;
-        background-opacity = 0.8;
-        background-blur-radius = 20;
-        cursor-click-to-move = true;
-        auto-update = "check"; # Let Nix handle this while we use ghostty-bin
-      }
-      // lib.optionalAttrs pkgs.stdenv.isDarwin {
-        # keybind = [
-        #   "alt+left=unbind"
-        #   "alt+right=unbind"
-        # ];
-        macos-option-as-alt = "left";
-      }
-      // lib.optionalAttrs config.programs.fish.enable {
-        command = "${config.programs.fish.package}/bin/fish";
-      };
-  };
+      settings =
+        {
+          theme = "catppuccin-mocha";
+          font-family = "Iosevka Term Slab Custom";
+          font-size = 14;
+          background-opacity = 0.8;
+          background-blur-radius = 20;
+          cursor-click-to-move = true;
+          auto-update = "check"; # Let Nix handle this while we use ghostty-bin
+        }
+        // lib.optionalAttrs pkgs.stdenv.isDarwin {
+          # keybind = [
+          #   "alt+left=unbind"
+          #   "alt+right=unbind"
+          # ];
+          macos-option-as-alt = "left";
+        }
+        // lib.optionalAttrs config.programs.fish.enable {
+          command = "${config.programs.fish.package}/bin/fish";
+        };
+    }
+    // lib.optionalAttrs pkgs.stdenv.isDarwin {
+      package = pkgs.ghostty-bin;
+    };
 }
