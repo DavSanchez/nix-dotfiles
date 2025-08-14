b078ed67090c3e4da3e937a0f1b335677b5f03ca
David Sánchez <davidslt+git@pm.me>
2025-08-14 22:06:32 +0100

    feat: tmuxify ghostty

--- home-manager/features/terminals/ghostty.nix
+++ home-manager/features/terminals/ghostty.nix
@@ -22,6 +22,30 @@
       background-blur-radius = 20;
       cursor-click-to-move = true;
       auto-update = "check"; # Let Nix handle this while we use ghostty-bin
+      # tmux-ifying ghostty!
+      mouse-hide-while-typing = true;
+      scrollback-limit = 1000000;
+      window-save-state = "always";
+      keybind = [
+        "ctrl+h=goto_split:left"
+        "ctrl+j=goto_split:bottom"
+        "ctrl+k=goto_split:top"
+        "ctrl+l=goto_split:right"
+
+        "ctrl+b>h=new_split:left"
+        "ctrl+b>j=new_split:down"
+        "ctrl+b>k=new_split:up"
+        "ctrl+b>l=new_split:right"
+        "ctrl+b>f=toggle_split_zoom"
+
+        "ctrl+b>n=next_tab"
+        "ctrl+b>p=previous_tab"
+
+        "super+r=reload_config"
+      ]
+      ++ lib.optionals (!pkgs.stdenv.isDarwin) [
+        "ctrl+n=new_window"
+      ];
     }
     // lib.optionalAttrs pkgs.stdenv.isDarwin {
       # keybind = [
