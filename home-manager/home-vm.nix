4b11504bf316fbeb9cc485bd2092f0ec4899211e
David Sánchez <davidslt+git@pm.me>
2025-08-17 02:27:37 +0100

    chore(nixvim): replace homeManagerModules with homeModules

--- home-manager/home-vm.nix
+++ home-manager/home-vm.nix
@@ -9,7 +9,7 @@
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
-    inputs.nixvim.homeManagerModules.nixvim
+    inputs.nixvim.homeModules.nixvim
     inputs.catppuccin.homeModules.catppuccin
 
     # You can also split up your configuration and import pieces of it here:
