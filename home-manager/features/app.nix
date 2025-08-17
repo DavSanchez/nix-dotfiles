99ce4e1c67ec258f1cc2eb8ded1f5eaab8c26dff
David Sánchez <davidslt+git@pm.me>
2025-08-17 13:15:30 +0100

    chore: add anytype

--- home-manager/features/app.nix
+++ home-manager/features/app.nix
@@ -14,6 +14,8 @@
       # mtr-gui
       czkawka # Multi functional app to find duplicates, empty folders, similar images etc
       zathura
+
+      anytype # P2P note-taking app
     ])
     ++ lib.optionals pkgs.stdenv.isDarwin [ pkgs.utm ]
     ++ lib.optionals pkgs.stdenv.isLinux [ pkgs.imhex ];
