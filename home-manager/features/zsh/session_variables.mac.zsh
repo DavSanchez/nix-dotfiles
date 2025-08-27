c261559131a0289f113f0177e1fb234efdf9da86
David Sánchez <davidslt+git@pm.me>
2025-08-28 00:32:24 +0100

    chore(zsh): remove unused scripts

--- home-manager/features/zsh/session_variables.mac.zsh
+++
@@ -1,9 +0,0 @@
-# export SSL_CERT_FILE=~/.nix-profile/etc/ssl/certs/ca-bundle.crt
-
-# $(brew --prefix)/bin is mac specific and where brew installs stuff. As we are
-# making use of brew as fallback so we need to add it
-
-# export PATH="$HOME/.local/bin:$HOME/.nix-profile/bin:$(brew --prefix)/bin:$PATH"
-
-# Handling Haskell via Homebrew + GHCup in macOS until behavior is stabilized
-# export PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"
