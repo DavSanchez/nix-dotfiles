da99c7a378c01e4f4ecf11c06e08567b4b2eca2a
David Sánchez <davidslt+git@pm.me>
2025-08-28 00:32:36 +0100

    feat(nu): setup

--- home-manager/features/nu/default.nix
+++ home-manager/features/nu/default.nix
@@ -9,69 +9,49 @@
     enable = true;
     environmentVariables = {
       DOTFILES = "${config.home.homeDirectory}/.dotfiles";
+      EDITOR = "${pkgs.helix}/bin/hx";
     };
 
-    # Other options:
     # configFile = ...;
     # envFile = ...;
     # loginFile = ...;
 
     extraConfig = ''
-      # Theme from nu_scripts: catppuccin mocha
-      source ${pkgs.nu_scripts}/share/nu_scripts/themes/nu-themes/catppuccin-mocha.nu
-
       # Aliases from nu_scripts
       source ${pkgs.nu_scripts}/share/nu_scripts/aliases/bat/bat-aliases.nu
+      source ${pkgs.nu_scripts}/share/nu_scripts/aliases/eza/eza-aliases.nu
       source ${pkgs.nu_scripts}/share/nu_scripts/aliases/docker/docker-aliases.nu
       source ${pkgs.nu_scripts}/share/nu_scripts/aliases/git/git-aliases.nu
 
       # Custom completions
       # TODO
     '';
-    extraEnv = '''';
+    # extraEnv = ...;
     # extraLogin = ...;
 
-    # FIXME: forgit is not available
-    shellAliases = {
-      # git-forgit
-      gflo = "forgit_log";
-      gfrl = "forgit_reflog";
-      gfd = "forgit_diff";
-      gfa = "forgit_add";
-      gfrh = "forgit_reset_head";
-      gfi = "forgit_ignore";
-      gfcf = "forgit_checkout_file";
-      gfcb = "forgit_checkout_branch";
-      gfbd = "forgit_branch_delete";
-      gfct = "forgit_checkout_tag";
-      gfco = "forgit_checkout_commit";
-      gfrc = "forgit_revert_commit";
-      gfclean = "forgit_clean";
-      gfss = "forgit_stash_show";
-      gfsp = "forgit_stash_push";
-      gfcp = "forgit_cherry_pick";
-      gfrb = "forgit_rebase";
-      gfbl = "forgit_blame";
-      gffu = "forgit_fixup";
-    }
-    // lib.optionalAttrs pkgs.stdenv.isDarwin {
+    shellAliases = lib.optionalAttrs pkgs.stdenv.isDarwin {
+      nu-open = "open";
       open = "^open"; # ^ escapes the command and invokes the operating system one
     };
 
-    # TODO enable this
-    # plugins =
-    #   with pkgs.nushellPlugins;
-    #   [
-    #     net
-    #     skim
-    #     units
-    #     query
-    #     gstat
-    #     polars
-    #     formats
-    #     highlight
-    #   ]
-    #   ++ lib.optionals pkgs.stdenv.isLinux [ dbus ];
+    settings = {
+      highlight_resolved_externals = true;
+    };
+
+    plugins =
+      with pkgs.nushellPlugins;
+      [
+        # net # currently broken (not compatible with nu version)
+        skim
+        # units # currently broken (not compatible with nu version)
+        query
+        gstat
+        polars
+        # semver # not available for macOS
+        formats
+        highlight
+      ]
+      ++ lib.optionals pkgs.stdenv.isLinux [ dbus ];
   };
 
   home.packages = with pkgs; [ nufmt ];
