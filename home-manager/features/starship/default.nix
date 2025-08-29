b7c8ee3ffc5c9bec566d7a8338a2010c30161990
David Sánchez <davidslt+git@pm.me>
2025-08-30 00:21:05 +0100

    style(starship): change coloring to match catppuccin

--- home-manager/features/starship/default.nix
+++ home-manager/features/starship/default.nix
@@ -5,20 +5,14 @@
 
     settings = {
       format = lib.concatStrings [
-        "[>>-](bright-black) $all"
+        "[>>-](lavender) $all"
         "$fill"
-        "$shell$shlvl$time"
+        "$shell$shlvl"
         "$line_break"
-        "[>>-](bright-black) $character"
+        "[>>-](lavender) $character"
       ];
       add_newline = true;
       fill.symbol = " ";
-      time = {
-        disabled = false;
-        format = "[ $time](fg:blue bold)";
-        time_format = "%F %R";
-      };
-
       shell = {
         style = "cyan bold";
         disabled = false;
