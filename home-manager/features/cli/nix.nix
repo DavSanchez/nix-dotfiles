09eee0e771d20fd6fcc241942f973e84b60242a4
David Sánchez <davidslt+git@pm.me>
2025-10-15 14:53:34 +0100

    chore: clean some unused programs

--- home-manager/features/cli/nix.nix
+++ home-manager/features/cli/nix.nix
@@ -13,13 +13,13 @@
     nix-output-monitor
     nix-update
     nix-diff
-    statix # Lints and suggestions for Nix
+    # statix # Lints and suggestions for Nix
     comma # Runs programs without installing them
     cachix
     nurl
     nix-init # Nix derivation boilerplate
-    deadnix # Scan Nix files for dead code
-    vulnix # Vulnerability (CVE) scanner for Nix
+    # deadnix # Scan Nix files for dead code
+    # vulnix # Vulnerability (CVE) scanner for Nix
     nix-tree # Interactively browse a Nix store paths dependencies
     # nix-du # Visualize gc-roots
     nix-melt # Ranger for flake.lock
