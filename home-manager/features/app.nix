b961e031d7e8c81f8d391c25a249bd00884810d0
David Sánchez <davidslt+git@pm.me>
2025-12-11 17:22:18 +0000

    feat: conditional macos services for raycast and alt-tab

--- home-manager/features/app.nix
+++ home-manager/features/app.nix
@@ -1,35 +1,65 @@
-{ lib, pkgs, ... }:
 {
-  home.packages =
-    (with pkgs; [
-      sniffnet
-      # zotero
-      # mtr-gui # Already using mtr
-      czkawka # Multi functional app to find duplicates, empty folders, similar images etc
-      zathura
-      imhex
-      discord
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+lib.mkMerge [
+  {
+    home.packages =
+      (with pkgs; [
+        sniffnet
+        # zotero
+        # mtr-gui # Already using mtr
+        czkawka # Multi functional app to find duplicates, empty folders, similar images etc
+        zathura
+        imhex
+        discord
+      ])
+      ++ lib.optionals pkgs.stdenv.isLinux (
+        with pkgs;
+        [
+          handbrake
+          gqrx
+          vlc
+          sonic-pi
+          # logseq
+          anytype # P2P note-taking app
+          # yacreader
+        ]
+      );
+  }
+  (lib.mkIf pkgs.stdenv.isDarwin {
+    home.packages = with pkgs; [
+      utm
+      iina
+      # ice-bar # Waiting for Tahoe compat
+      raycast
+      alt-tab-macos
+    ];
 
-    ])
-    ++ lib.optionals pkgs.stdenv.isDarwin (
-      with pkgs;
-      [
-        utm
-        chatgpt
-        iina
-        # ice-bar # Waiting for Tahoe compat
-      ]
-    )
-    ++ lib.optionals pkgs.stdenv.isLinux (
-      with pkgs;
-      [
-        handbrake
-        gqrx
-        vlc
-        sonic-pi
-        # logseq
-        # anytype # P2P note-taking app
-        # yacreader
-      ]
-    );
-}
+    # This depends on having `raycast` installed. See `home.packages` above.
+    launchd.agents.raycast = {
+      config = {
+        Program = builtins.toString (
+          lib.path.append (
+            /. + config.home.homeDirectory
+          ) "Applications/Home Manager Apps/Raycast.app/Contents/MacOS/Raycast"
+        );
+        RunAtLoad = true;
+      };
+    };
+
+    # This depends on having `alt-tab-macos` installed. See `home.packages` above.
+    launchd.agents.alt-tab = {
+      config = {
+        Program = builtins.toString (
+          lib.path.append (
+            /. + config.home.homeDirectory
+          ) "Applications/Home Manager Apps/AltTab.app/Contents/MacOS/AltTab"
+        );
+        RunAtLoad = true;
+      };
+    };
+  })
+]
