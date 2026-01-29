fad6686b4ec480bafdf65c687039dd7b16be11cc
David Sánchez <davidslt+git@pm.me>
2026-01-29 20:46:32 +0000

    chore: tealdeer config

--- home-manager/features/cli/terminal.nix
+++ home-manager/features/cli/terminal.nix
@@ -85,10 +85,6 @@
     tealdeer = {
       enable = true;
       settings = {
-        display = {
-          compact = true;
-          # use_pager = true;
-        };
         updates = {
           auto_update = true;
         };
