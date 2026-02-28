6ce76f4947350717bfa69c6294e67269c22b1461
David Sánchez <davidslt+git@pm.me>
2026-02-28 02:46:06 +0000

    chore(haskell): remove duplicate libs for now

--- home-manager/features/cli/documents.nix
+++ home-manager/features/cli/documents.nix
@@ -5,10 +5,6 @@
     ## Typesetting (with pygments for minted, pygmentex...)
     # texlive.combined.scheme-full
     typst
-    python311Packages.pygments
-    haskellPackages.pandoc-crossref
-
-    haskellPackages.patat # Terminal-based presentations
   ];
 
   programs = {
--- home-manager/features/dev/haskell/default.nix
+++ home-manager/features/dev/haskell/default.nix
@@ -1,53 +1,18 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 {
   home = {
     packages = with pkgs.haskellPackages; [
       ghc
-
-      ormolu
 
       cabal-install
       cabal-gild
       # cabal-add
       # cabal-audit
 
-      haskell-language-server # Langserver
+      # haskell-language-server # Langserver
       ghcid
-      ghcide
 
-      hoogle
+      # hoogle
     ];
-
-    # hoogle ghci integration
-    # example> :hoogle <$>
-    file.".ghci" = {
-      enable = false;
-      text = ''
-        :def hoogle \x -> return $ ":!hoogle \"" ++ x ++ "\""
-        :def doc \x -> return $ ":!hoogle --info \"" ++ x ++ "\""
-      '';
-    };
-    # https://docs.haskellstack.org/en/stable/yaml_configuration/#non-project-specific-config
-    file.".stack/config.yaml" = {
-      enable = false;
-      text = lib.generators.toYAML { } {
-        system-ghc = true;
-        install-ghc = false;
-        templates = {
-          scm-init = "git";
-          params = {
-            author-name = "David Sánchez"; # config.programs.git.userName;
-            author-email = "davidslt@pm.me"; # config.programs.git.userEmail;
-            github-username = "DavSanchez";
-          };
-        };
-        nix.enable = true;
-      };
-    };
-
-    file.".summoner.toml" = {
-      enable = false;
-      source = ./summoner.toml;
-    };
   };
 }
