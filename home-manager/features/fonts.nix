63286e34092049ea65ab34ac03ff51f1f9626f98
David Sánchez <davidslt+git@pm.me>
2026-03-04 23:50:26 +0000

    feat: fonts

--- home-manager/features/fonts.nix
+++ home-manager/features/fonts.nix
@@ -3,13 +3,18 @@
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
+    iosevka
     nerd-fonts.iosevka
     nerd-fonts.iosevka-term
     nerd-fonts.iosevka-term-slab
-    
+
     fira-code-symbols
     nerd-fonts.fira-code
+
+    jetbrains-mono
     nerd-fonts.jetbrains-mono
+
+    monaspace
     nerd-fonts.monaspace
 
     maple-mono.NF
