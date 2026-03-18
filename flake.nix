74ede4cff98fb8b8adee00d81065ad483061ea05
David Sánchez <davidslt+git@pm.me>
2026-03-18 16:09:17 +0000

    chore: remove x86_64-darwin system

--- flake.nix
+++ flake.nix
@@ -55,7 +55,6 @@
         "i686-linux"
         "x86_64-linux"
         "aarch64-darwin"
-        "x86_64-darwin"
       ];
       # This is a function that generates an attribute by calling a function you
       # pass to it, with each system as an argument
