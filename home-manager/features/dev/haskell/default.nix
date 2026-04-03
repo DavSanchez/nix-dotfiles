05447ac062dc59510591a1ba7f6fe1eb468e961b
David Sánchez <davidslt+git@pm.me>
2026-04-03 02:52:07 +0100

    style: fmt

--- home-manager/features/dev/haskell/default.nix
+++ home-manager/features/dev/haskell/default.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:
 {
   home = {
-    packages = (with pkgs; [
-      ghc
-      cabal-install
-      haskell-language-server
-      ghcid
-    ]) ++ (with pkgs.haskellPackages; [
-      cabal-gild
-      cabal-add
-      # cabal-audit
-      hoogle
-    ]);
+    packages =
+      (with pkgs; [
+        ghc
+        cabal-install
+        haskell-language-server
+        ghcid
+      ])
+      ++ (with pkgs.haskellPackages; [
+        cabal-gild
+        cabal-add
+        # cabal-audit
+        hoogle
+      ]);
   };
 }
