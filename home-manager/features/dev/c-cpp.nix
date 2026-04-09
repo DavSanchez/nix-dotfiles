b8feed1f33d537d1e9e17957c6d4024fbe8e5d3a
David Sánchez <davidslt+git@pm.me>
2026-04-10 00:19:03 +0100

    feat(dev): lldb and zig

--- home-manager/features/dev/c-cpp.nix
+++ home-manager/features/dev/c-cpp.nix
@@ -17,6 +17,6 @@
       gcc
 
       # llvm
-      lldb
+      # lldb
     ];
 }
--- home-manager/features/dev/rust.nix
+++ home-manager/features/dev/rust.nix
@@ -4,8 +4,13 @@
     packages = with pkgs; [
       rustup
 
+      # debugging
+      lldb
+
       cargo-zigbuild # cross-compilation via zig
       zig # needed by cargo-zigbuild
+      zls # just go with everything
+      
       cargo-cache
       cargo-nextest
       bacon # background rust code checker
