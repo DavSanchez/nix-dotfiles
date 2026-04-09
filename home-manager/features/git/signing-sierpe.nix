af8df7148489c7b914645552717e7496dc6d1e00
David Sánchez <davidslt+git@pm.me>
2026-04-10 00:56:23 +0100

    feat: jj signing

--- home-manager/features/git/signing-sierpe.nix
+++ home-manager/features/git/signing-sierpe.nix
@@ -1,9 +1,13 @@
 _: {
-  programs.git = {
-    # Move to localconf?
-    signing = {
+  programs = {
+    git.signing = {
       key = "03747679A51FECE9";
       signByDefault = true;
+    };
+    jujutsu.settings.signing = {
+      behavior = "own";
+      backend = "gpg";
+      key = "03747679A51FECE9";
     };
   };
 }
