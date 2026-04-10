4c8a4b3ea3fdec2b0620c137a013912f3669722e
David Sánchez <11422515+DavSanchez@users.noreply.github.com>
2026-04-10 01:03:24 +0100

    feat: jj signing (#273)

--- home-manager/features/git/signing-nr.nix
+++ home-manager/features/git/signing-nr.nix
@@ -1,9 +1,13 @@
 _: {
-  programs.git = {
-    # Move to localconf?
-    signing = {
+  programs = {
+    git.signing = {
       key = "5485C37B9941B0BF";
       signByDefault = true;
+    };
+    jujutsu.settings.signing = {
+      behavior = "own";
+      backend = "gpg";
+      key = "5485C37B9941B0BF";
     };
   };
 }
--- home-manager/features/git/signing-solio.nix
+++ home-manager/features/git/signing-solio.nix
@@ -1,9 +1,13 @@
 _: {
-  programs.git = {
-    # Move to localconf?
-    signing = {
+  programs = {
+    git.signing = {
       key = "FB27FC70B15E6D6C";
       signByDefault = true;
+    };
+    jujutsu.settings.signing = {
+      behavior = "own";
+      backend = "gpg";
+      key = "FB27FC70B15E6D6C";
     };
   };
 }
