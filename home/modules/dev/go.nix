5893e565bb7b3716981da79d9d942ef77ccbbb62
David Sánchez <davidslt+git@pm.me>
2026-04-23 12:44:29 +0100

    feat: go tools

--- home/modules/dev/go.nix
+++ home/modules/dev/go.nix
@@ -7,17 +7,31 @@
 {
   programs.go = {
     enable = true;
-    env.GOPATH = builtins.toString (lib.path.append (/. + config.home.homeDirectory) ".go");
+    env.GOPATH = toString (lib.path.append (/. + config.home.homeDirectory) ".go");
   };
 
   home.packages = with pkgs; [
-    gopls # Langserver
-    delve
+    # gopls # Langserver
+    # langserver but removing modernize. REVIEW: when NixOS/nixpkgs#509480 gets addressed
+    (pkgs.symlinkJoin {
+      name = "gopls-sans-modernize";
+      paths = [ gopls ];
+      postBuild = ''
+        rm -f "$out/bin/modernize"
+      '';
+    })
+    capslock
+    go-task
     gotools
-    gomodifytags
-    impl
     go-tools
+    gofumpt
+    gomodifytags
     gotests
+    gore
+    godef
+    delve
+    gdlv
+    impl
     golangci-lint
   ];
 }
