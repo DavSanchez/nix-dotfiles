fe0792e7889c49dc183848d0668ca6e6a9fb9f67
David Sánchez <davidslt+git@pm.me>
2026-04-24 22:50:19 +0100

    feat: zed settings

--- home/modules/zed.nix
+++ home/modules/zed.nix
@@ -8,22 +8,65 @@ _: {
       "rust"
       "go"
       "opencode"
-      # "catppuccin-blur"
     ];
     # userKeymaps = { };
     userSettings = {
-      auto_update = false;
-      helix_mode = true;
-      hour_format = "hour24";
-      buffer_font_family = "Iosevka Slab";
-      option_as_meta = false;
-      load_direnv = "shell_hook";
-      show_whitespaces = "all";
-      autosave = {
-        after_delay.milliseconds = 1000;
+      terminal = {
+        # font_size= 14.0;
+        font_family = "Iosevka Term Slab";
+        font_fallbacks = [
+          "FiraCode"
+          "Menlo"
+          "Monaco"
+          "Courier New"
+          "monospace"
+        ];
+        max_scroll_history_lines = 5000;
       };
-      relative_line_numbers = true;
+      git = {
+        inline_blame = {
+          enabled = true;
+        };
+      };
+      base_keymap = "VSCode"; # default
+      git_panel = {
+        fallback_branch_name = "master";
+      };
+      # buffer_font_size = 14.0;
+      buffer_font_fallbacks = [
+        "FiraCode"
+        "Menlo"
+        "Monaco"
+        "Courier New"
+        "monospace"
+      ];
+      file_types = {
+        haskell = [
+          "*.tidal"
+        ];
+      };
+      colorize_brackets = true;
+      auto_indent_on_paste = true;
+      show_edit_predictions = true;
+      semantic_tokens = "full";
+      # wrap_guides = [ ];
+      soft_wrap = "editor_width";
+      # tab_size = 2;
+      autosave = {
+        after_delay = {
+          milliseconds = 1000;
+        };
+      };
+      buffer_font_family = "Iosevka Slab";
+      helix_mode = true;
+      load_direnv = "shell_hook";
+      relative_line_numbers = "enabled";
+      show_whitespaces = "all";
+      auto_update = false;
+      hour_format = "hour24";
+      option_as_meta = false;
     };
+
     # extraPackages = [ ];
     installRemoteServer = true;
   };
