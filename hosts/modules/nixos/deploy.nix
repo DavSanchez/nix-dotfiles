87f86f131273b9245f56522861c00a4ab127fa23
David Sánchez <davidslt+git@pm.me>
2026-04-26 05:11:47 +0100

    fix(deploy): remove deploy user for now
    somehow setting this user prevents SSH to my other account. Is this
    because they share the same authorized keys?

--- hosts/modules/nixos/deploy.nix
+++ hosts/modules/nixos/deploy.nix
@@ -1,26 +1,26 @@
 _: {
-  users.users.deploy = {
-    isNormalUser = true;
-    description = "System deploy user";
-    uid = 2000;
-    extraGroups = [
-      "wheel"
-      "sudo"
-    ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvM06bcMBkqNyadDKDGQXl4ztggBM1mgg5/CLqnqNvn davidslt+ssh@pm.me"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVmyCytQRBGJMOdUawPjHZvHGfjovgHdq5o6hjmlW+P davidslt+ssh@pm.me"
-    ];
-  };
+  # users.users.deploy = {
+  #   isNormalUser = true;
+  #   description = "System deploy user";
+  #   uid = 2000;
+  #   extraGroups = [
+  #     "wheel"
+  #     "sudo"
+  #   ];
+  #   openssh.authorizedKeys.keys = [
+  #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvM06bcMBkqNyadDKDGQXl4ztggBM1mgg5/CLqnqNvn davidslt+ssh@pm.me"
+  #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVmyCytQRBGJMOdUawPjHZvHGfjovgHdq5o6hjmlW+P davidslt+ssh@pm.me"
+  #   ];
+  # };
 
-  services.openssh.settings.AllowUsers = [ "deploy" ];
+  # services.openssh.settings.AllowUsers = [ "deploy" ];
 
-  # Enable 'sudo' with SSH key
-  security.pam.sshAgentAuth = {
-    enable = true;
-  };
+  # # Enable 'sudo' with SSH key
+  # security.pam.sshAgentAuth = {
+  #   enable = true;
+  # };
 
-  nix.settings.trusted-users = [ "deploy" ];
+  # nix.settings.trusted-users = [ "deploy" ];
 
   # Make deployments to this machine passwordless
   security.sudo.wheelNeedsPassword = false;
