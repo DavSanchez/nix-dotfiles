1ae3b5b75aaae071414800a763a24dedb8ee8cd0
David Sánchez <davidslt+git@pm.me>
2026-04-27 02:50:27 +0100

    feat(vscode): disable automatic updates

--- home/modules/vscode/default.nix
+++ home/modules/vscode/default.nix
@@ -6,17 +6,16 @@
   programs.vscode = {
     enable = true;
     # package = pkgs.vscodium; # vscodium.fhs for complex extensions?
-    mutableExtensionsDir = true;
 
     profiles.default = {
-      enableUpdateCheck = true;
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
       userSettings = (import ./settings.nix) pkgs; # Pass pkgs to reference paths
       # Extra keybindings leveraging hyper key (to modify)
       keybindings = import ./keybindings.nix;
       # Extension issues and other documentation:
       # https://nixos.wiki/wiki/VSCodium
       extensions = (import ./extensions.nix) pkgs;
-      enableExtensionUpdateCheck = true;
     };
   };
 }
