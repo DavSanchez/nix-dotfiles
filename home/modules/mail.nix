2c7faf08d5317ddfb91a3fbd8a4e63f8c1fda7d7
David Sánchez <davidslt+git@pm.me>
2026-04-28 18:08:55 +0100

    feat: prepare protonmail bridge

--- home/modules/mail.nix
+++ home/modules/mail.nix
@@ -1,11 +1,19 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  ...
+}:
 {
   home.packages = with pkgs; [
     protonmail-desktop
     protonmail-bridge
   ];
+
   programs.himalaya = {
     enable = true;
     # setings = { };
+  };
+
+  services.protonmail-bridge = {
+    enable = pkgs.stdenv.isLinux;
   };
 }
