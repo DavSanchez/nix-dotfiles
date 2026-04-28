7071202d1881dcdcf0c0ce3a2e49f5fee715394e
David Sánchez <davidslt+git@pm.me>
2026-04-29 00:22:14 +0100

    feat(home): protonmail bridge darwin

--- home/modules/mail.nix
+++ home/modules/mail.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   ...
 }:
 {
@@ -10,10 +11,35 @@
 
   programs.himalaya = {
     enable = true;
-    # setings = { };
+    # settings = { };
   };
 
+  # ───────────────────────────────────────────────
+  # Linux
+  # ───────────────────────────────────────────────
   services.protonmail-bridge = {
     enable = pkgs.stdenv.isLinux;
+  };
+
+  # ───────────────────────────────────────────────
+  # Darwin (until upstream home-manager adds launchd support)
+  # ───────────────────────────────────────────────
+  launchd.agents.protonmail-bridge = lib.mkIf pkgs.stdenv.isDarwin {
+    enable = true;
+    config = {
+      ProgramArguments = [
+        (lib.getExe pkgs.protonmail-bridge)
+        "--noninteractive"
+        # ]
+        # ++ lib.optionals (cfg.logLevel != null) [
+        #   "--log-level"
+        #   cfg.logLevel
+      ];
+      KeepAlive = true;
+      RunAtLoad = true;
+      ProcessType = "Background";
+      StandardOutPath = "/tmp/protonmail-bridge.log";
+      StandardErrorPath = "/tmp/protonmail-bridge.err.log";
+    };
   };
 }
