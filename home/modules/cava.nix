c99ee653fdc025fcedb43a2a1e90670382187fa8
David Sánchez <davidslt+git@pm.me>
2026-04-28 17:35:47 +0100

    feat: cava on linux only

--- home/modules/cava.nix
+++ home/modules/cava.nix
@@ -1,17 +1,6 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 {
   programs.cava = {
-    enable = false;
-    settings =
-      { }
-      // lib.optionalAttrs pkgs.stdenv.isDarwin {
-        # Relies on having Background Music installed!
-        # Done in hosts/darwin for each host
-        # <https://github.com/karlstav/cava?tab=readme-ov-file#macos-1>
-        input = {
-          method = "portaudio";
-          source = "Background Music";
-        };
-      };
+    enable = pkgs.stdenv.isLinux;
   };
 }
