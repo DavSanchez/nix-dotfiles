c49ad340e2b9f7a7f4a62f6a98cc54535273a551
David Sánchez <davidslt+git@pm.me>
2026-06-08 12:42:07 +0100

    fix(dix): disable failing test on darwin (temp)

--- home/modules/cli/nix.nix
+++ home/modules/cli/nix.nix
@@ -14,7 +14,25 @@
     nix-output-monitor
     nix-update
     nix-diff
-    dix
+    # TODO: Remove this override once `dix` releases a new version
+    (dix.overrideAttrs (prev: {
+      checkFlags =
+        prev.checkFlags
+        ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+          "--skip=store::test_utils::tests::test_circular_dependencies"
+          "--skip=store::test_utils::tests::test_db_query_closure_path_info"
+          "--skip=store::test_utils::tests::test_db_query_closure_size"
+          "--skip=store::test_utils::tests::test_db_query_dependents"
+          "--skip=store::test_utils::tests::test_db_query_system_derivations"
+          "--skip=store::test_utils::tests::test_deep_chain"
+          "--skip=store::test_utils::tests::test_self_referential_path"
+          "--skip=store::test_utils::tests::test_wide_tree"
+          "--skip=tests::test_path_to_canonical_string_basic"
+          "--skip=tests::test_path_to_canonical_string_invalid_unicode"
+          "--skip=tests::test_path_to_canonical_string_with_symlink"
+          "--skip=version::tests::test_version_transitivity"
+        ];
+    }))
     # statix # Lints and suggestions for Nix
     comma # Runs programs without installing them
     cachix
