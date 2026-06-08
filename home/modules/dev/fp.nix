f9240837367a6cb12bd6770b88ad3e1507e684fe
David Sánchez <davidslt+git@pm.me>
2026-06-08 12:57:30 +0100

    chore: deactivate unused langs

--- home/modules/dev/fp.nix
+++ home/modules/dev/fp.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    idris2 # FP with dependent types, compiles to C
+    # idris2 # FP with dependent types, compiles to C
     # idris2Packages.idris2Lsp # Langserver
 
-    agda # FP with dependent types, compiles to Haskell
+    # agda # FP with dependent types, compiles to Haskell
     # haskellPackages.agda-language-server # Langserver
 
     elixir # Dynamic FP for the Erlang VM
@@ -14,7 +14,7 @@
     # fstar # ML-like FP language aimed at program verification
     # elmPackages.elm # FP for the frontend
 
-    purescript # FP for the frontend
+    # purescript # FP for the frontend
     # nodePackages.purescript-language-server # Langserver (unmaintained)
 
     clojure # FP for the JVM, lisp-like
@@ -25,6 +25,6 @@
     flix # FP for the JVM
     # roc.cli # Roc programming lang, comes from an overlay
     # grain # FP for the web
-    unison-ucm # distributed, content-addressed FP
+    # unison-ucm # distributed, content-addressed FP
   ];
 }
