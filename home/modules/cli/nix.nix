71fe26d0c0fd53b1afa3f04c8b27b419a8f5e207
David Sánchez <davidslt+git@pm.me>
2026-06-12 13:32:56 +0100

    fix: remove dix override

--- home/modules/cli/nix.nix
+++ home/modules/cli/nix.nix
@@ -14,34 +14,16 @@
     nix-output-monitor
     nix-update
     nix-diff
-    # TODO: Remove this override once `dix` releases a new version
-    (dix.overrideAttrs (prev: {
-      checkFlags =
-        prev.checkFlags
-        ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-          "--skip=store::test_utils::tests::test_circular_dependencies"
-          "--skip=store::test_utils::tests::test_db_query_closure_path_info"
-          "--skip=store::test_utils::tests::test_db_query_closure_size"
-          "--skip=store::test_utils::tests::test_db_query_dependents"
-          "--skip=store::test_utils::tests::test_db_query_system_derivations"
-          "--skip=store::test_utils::tests::test_deep_chain"
-          "--skip=store::test_utils::tests::test_self_referential_path"
-          "--skip=store::test_utils::tests::test_wide_tree"
-          "--skip=tests::test_path_to_canonical_string_basic"
-          "--skip=tests::test_path_to_canonical_string_invalid_unicode"
-          "--skip=tests::test_path_to_canonical_string_with_symlink"
-          "--skip=version::tests::test_version_transitivity"
-        ];
-    }))
-    # statix # Lints and suggestions for Nix
+    dix
+    statix # Lints and suggestions for Nix
     comma # Runs programs without installing them
     cachix
     nurl
     nix-init # Nix derivation boilerplate
-    # deadnix # Scan Nix files for dead code
-    # vulnix # Vulnerability (CVE) scanner for Nix
+    deadnix # Scan Nix files for dead code
+    vulnix # Vulnerability (CVE) scanner for Nix
     nix-tree # Interactively browse a Nix store paths dependencies
-    # nix-du # Visualize gc-roots
+    nix-du # Visualize gc-roots
     nix-melt # Ranger for flake.lock
 
     # Binary management
