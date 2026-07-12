e7013e7da2c36c3582bb2d4b601e0afc6bd13b82
David Sánchez <davidslt+git@pm.me>
2026-07-12 23:12:09 +0100

    chore: omniwm 0.5.6

--- pkgs/omniwm.nix
+++ pkgs/omniwm.nix
@@ -7,11 +7,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "omniwm";
-  version = "0.5.5";
+  version = "0.5.6";
 
   src = fetchurl {
     url = "https://github.com/BarutSRB/OmniWM/releases/download/v${finalAttrs.version}/OmniWM-v${finalAttrs.version}.zip";
-    hash = "sha256-9JEvkAV7KRPgejx88ycdzDE+maLNFh5PDz0Gp4LaZjI=";
+    hash = "sha256-GjNl1iXCEjjGMU17F3V3Je/c3fTiwjFHwJzzJJcDo6U=";
   };
 
   dontUnpack = true;
